b9bbb21f16d9a631478026633b4d43a86e586662
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:08:41 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - Lab/09. Yard Greening/YardGreening.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    internal class YardGreening
+    {
+        static void Main(string[] args)
+        {
+            double squareMetersForGreening = double.Parse(Console.ReadLine());
+            double priceForOneSquereMeter = 7.61;
+            double discout = 0.18; // Остъпката е 18%
+            double price = squareMetersForGreening * priceForOneSquereMeter;
+            double priceWithDiscount = price * discout;
+            double finalPrice = price - priceWithDiscount;
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discout is: {priceWithDiscount}");
+        }
+    }
+}
