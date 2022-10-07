5f3e433e1130d235ab98cc2491b1a3911c56848b
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:11:34 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - Exercises/01. USD to BGN/USDToBGN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._USD_to_BGN
+{
+    internal class USDToBGN
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double oneUSDToBGN = 1.79549;
+            double bgn = usd * oneUSDToBGN;
+            Console.WriteLine(bgn);
+        }
+    }
+}
