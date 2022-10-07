748a126bbc8fb454961c7193c3bdc898e9c27ee6
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:08:07 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - Lab/08.PetShop/PetShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    internal class PetShop
+    {
+        static void Main(string[] args)
+        {
+            int numberPacketFoodForDogs = int.Parse(Console.ReadLine());
+            int numberPacketFoodForCats = int.Parse(Console.ReadLine());
+            double priceForOnePacketDogFood = 2.5;
+            double priceForOnePacketCatFood = 4;
+            double totalPrice = numberPacketFoodForDogs * priceForOnePacketDogFood + numberPacketFoodForCats * priceForOnePacketCatFood;
+            Console.WriteLine($"{totalPrice} lv.");
+        }
+    }
+}
