daeb5db075fcdaacbd2c2c3a2ae4cc90f2e27f89
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:07:24 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - Lab/07. Projects Creation/ProjectsCreation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07._Projects_Creation
+{
+    internal class ProjectsCreation
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int projectNumber = int.Parse(Console.ReadLine());
+            int timeForOneProject = 3;
+            int timeForProject = projectNumber * timeForOneProject;
+            Console.WriteLine($"The architect {architectName} will need {timeForProject} hours to complete {projectNumber} project/s.");
+        }
+    }
+}
