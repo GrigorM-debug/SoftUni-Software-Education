0dcc51e75b5c3b20b6136811fea616acccd15cb7
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:22:35 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - More Exercises/06. Fishland/Fishland.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._Fishland
+{
+    internal class Fishland
+    {
+        static void Main(string[] args)
+        {
+            //Вход
+            double mackarelOneKgPrice = double.Parse(Console.ReadLine());
+            double sprinkleOneKgPrice = double.Parse(Console.ReadLine());
+            double palamudKg = double.Parse(Console.ReadLine());
+            double safridKg = double.Parse(Console.ReadLine());
+            double musselsKg = double.Parse(Console.ReadLine());
+
+            //Изчисления
+            double palamudOneKgPrice = mackarelOneKgPrice + (mackarelOneKgPrice * 0.60);
+            double palamudPrice = palamudKg * palamudOneKgPrice;
+            double safridOneKgPrice = sprinkleOneKgPrice + (sprinkleOneKgPrice * 0.80);
+            double safridPrice = safridKg * safridOneKgPrice;
+            double musselsOneKgPrice = 7.50;
+            double musselsPrice = musselsKg * musselsOneKgPrice;
+            double finalSum = palamudPrice + safridPrice + musselsPrice;
+            
+            //Изход
+            Console.WriteLine("{0:F2}", finalSum);
+
+
+        }
+    }
+}
---
+++ C# - Programming Basics/First Steps In Coding - More Exercises/07. House Painting/House Painting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace _07._House_Painting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вход
+            double houseHeigh = double.Parse(Console.ReadLine());
+            double houseLenght = double.Parse(Console.ReadLine());
+            double triangleWallOnRoofHeigh = double.Parse(Console.ReadLine());
+            
+            //Изчисления стени
+            double sideWallArea = houseHeigh * houseLenght;
+            double windowArea = 1.5 * 1.5;
+            double sideWallsTotal = 2 * sideWallArea - 2 * windowArea;
+            double backWall = houseHeigh * houseHeigh;
+            double entrance = 1.2 * 2;
+            double frontAndBackWallTotal = 2 * backWall - entrance;
+            double totalArea = sideWallsTotal + frontAndBackWallTotal;
+            double greenPaint = totalArea / 3.4;
+
+            //Изчисления покрив
+            double twoRoofRectangle = 2 * (houseHeigh * houseLenght);
+            double twoTriangle = 2 * (houseHeigh * triangleWallOnRoofHeigh / 2);
+            double triangleTotal = twoRoofRectangle + twoTriangle;
+            double redPaint = triangleTotal / 4.3;
+
+            //Изход
+            Console.WriteLine("{0:F2}", greenPaint);
+            Console.WriteLine("{0:F2}", redPaint);
+        }
+    }
+}
