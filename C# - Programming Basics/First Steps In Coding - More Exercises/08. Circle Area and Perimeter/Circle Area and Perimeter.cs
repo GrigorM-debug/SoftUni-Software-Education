9f0b6363b1443da9245b9793dd4a7d50855e96ea
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:24:15 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - More Exercises/08. Circle Area and Perimeter/Circle Area and Perimeter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08._Circle_Area_and_Perimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console Input
+            double radius = double.Parse(Console.ReadLine());
+
+            //Calculations
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
+
+            //Console Output
+            Console.WriteLine("{0:F2}", area);
+            Console.WriteLine("{0:F2}", perimeter);
+        }
+    }
+}
