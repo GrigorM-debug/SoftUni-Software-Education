4afe9b61b3c9b1c2e21305815561746080a74cb7
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:20:06 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - More Exercises/03. Celsius to Fahrenheit/Celsius to Fahrenheit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03._Celsius_to_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ceisius = double.Parse(Console.ReadLine());
+            double fahrenheit = ceisius * 1.8 + 32;
+            Console.WriteLine("{0:F2}", fahrenheit);
+        }
+    }
+}
---
+++ C# - Programming Basics/First Steps In Coding - More Exercises/04. Vegetable Market/Vegetables Market.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Vegetable_Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вход 
+            double priceFoeOneKgVegetables = double.Parse(Console.ReadLine());
+            double priceForOneKgFruits = double.Parse(Console.ReadLine());
+            double totalKgVegetables = double.Parse(Console.ReadLine());
+            double totalKgFruits = double.Parse(Console.ReadLine());
+
+            //Изчисления
+            double vegetablesTotalPrice = priceFoeOneKgVegetables * totalKgVegetables;
+            double fruitsTotalPrice = priceForOneKgFruits * totalKgFruits;
+            double finalPriceInBGN = vegetablesTotalPrice + fruitsTotalPrice;
+            double finalPriceInEuro = finalPriceInBGN / 1.94;
+            Console.WriteLine("{0:F2}", finalPriceInEuro);
+
+        }
+    }
+}
