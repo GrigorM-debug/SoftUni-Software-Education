d3a491d6c6d4759431f67aa8ffd1d20e0175edf0
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:12:37 +0300

    Add files via upload

---
+++ C# - Programming Basics/First Steps In Coding - Exercises/03. Deposit Calculator/DepositCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    internal class DepositCalculator
+    {
+        static void Main(string[] args)
+        {
+            //Вход от конзолата
+            double depositSum = double.Parse(Console.ReadLine());
+            int mouths = int.Parse(Console.ReadLine());
+            double interestPercent = double.Parse(Console.ReadLine()) / 100;
+            //Действия
+            double calculatesTheAccruedInterest = (depositSum * interestPercent) / 12;
+            double depositGain = mouths * calculatesTheAccruedInterest;
+            double totalSum = depositSum + depositGain;
+            //Изход
+            Console.WriteLine(totalSum);
+        }
+    }
+}
