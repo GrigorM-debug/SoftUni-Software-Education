835e556a7858f13a61e9deab9fe7ec7339da6cb2
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-08 14:10:17 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements - Lab/01. Excellent Result/ExcellentResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Conditional_Statements___Lab
+{
+    internal class ExcellentResult
+    {
+        static void Main(string[] args)
+        {
+            double mark = double.Parse(Console.ReadLine());
+            if (mark >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
---
+++ C# - Programming Basics/Conditional Statements - Lab/04.PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04.PasswordGuess
+{
+    internal class PasswordGuess
+    {
+        static void Main(string[] args)
+        {
+            string inputPassword = Console.ReadLine();
+            string password = "s3cr3t!P@ssw0rd";
+
+            if (inputPassword == password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
