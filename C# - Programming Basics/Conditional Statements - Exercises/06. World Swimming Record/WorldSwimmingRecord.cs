1be0aa13473345b862c5c413416fa780e9e644e4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-12 21:49:26 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements - Exercises/06. World Swimming Record/WorldSwimmingRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace _06._World_Swimming_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double time1MInSeconds = double.Parse(Console.ReadLine());
+
+            double ivanSwimmedDistanceInSeconds = distanceInMeters * time1MInSeconds;
+            double slowTime = distanceInMeters / 15;
+            slowTime = Math.Floor(slowTime);
+            slowTime = slowTime * 12.5;
+
+            ivanSwimmedDistanceInSeconds = ivanSwimmedDistanceInSeconds + slowTime;
+
+            if (ivanSwimmedDistanceInSeconds >= recordInSeconds)
+            {
+                double timeDidntEnoughToBreakRec = ivanSwimmedDistanceInSeconds - recordInSeconds;
+                Console.WriteLine($"No, he failed! He was {(ivanSwimmedDistanceInSeconds - recordInSeconds):f2} seconds slower.");
+
+            }
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {(ivanSwimmedDistanceInSeconds):f2} seconds.");
+            }
+        }
+    }
+}
