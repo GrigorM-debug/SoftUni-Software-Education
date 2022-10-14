e4df17d65404b3fe476a58bc6ad6d3e6c19f60ee
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-14 17:15:41 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements - Exercises/07. Shopping/Shopping.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    internal class Shopping
+    {
+        static void Main(string[] args)
+        {
+            double budged = double.Parse(Console.ReadLine());
+            double gpuCnt = double.Parse(Console.ReadLine());   
+            double cpuCnt = double.Parse(Console.ReadLine());
+            double ramCnt = double.Parse(Console.ReadLine());
+
+            double oneGPUPrice = 250;
+            double gpuSum = gpuCnt * oneGPUPrice;
+            double cpuPrice = gpuSum * 0.35;
+            double cpuSum = cpuCnt * cpuPrice;
+            double ramPrice = gpuSum * 0.10;
+            double ramSum = ramCnt * ramPrice;
+            double totalSum = gpuSum + cpuSum + ramSum;
+
+            if (gpuCnt > cpuCnt)
+            {
+                totalSum = totalSum - totalSum * 0.15;
+            }
+            if (budged >= totalSum)
+            {
+                double moneyLeft = budged - totalSum;
+                Console.WriteLine($"You have {moneyLeft:f2} leva left!");
+            }
+            else if (totalSum > budged)
+            {
+                double moneyNeeded = totalSum - budged;
+                Console.WriteLine($"Not enough money! You need {moneyNeeded:f2} leva more!");
+            }
+
+        }
+    }
+}
---
+++ C# - Programming Basics/Conditional Statements - Exercises/08. Lunch Break/LunchBreak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace _08._Lunch_Break
+{
+    internal class LunchBreak
+    {
+        static void Main(string[] args)
+        {
+            string tvSeriesName = Console.ReadLine();
+            double episodeDuration = double.Parse(Console.ReadLine());
+            double breakDuration = double.Parse(Console.ReadLine());
+
+            double timeForLunch = breakDuration * 1 / 8;
+            double restTime = breakDuration * 1 / 4;
+            double leftTime = breakDuration - timeForLunch - restTime;
+
+            if (leftTime >= episodeDuration)
+            {
+                double freeTime = leftTime - episodeDuration;
+                Console.WriteLine($"You have enough time to wath {tvSeriesName} and left with {Math.Floor(freeTime)} minutes free time.");
+            }
+            else if (episodeDuration > leftTime)
+            {
+                double timeNeeded = episodeDuration - leftTime;
+                Console.WriteLine($"You don't have enough time to watch {tvSeriesName}, you need {Math.Ceiling(timeNeeded)} more minutes.");
+            }
+        }
+    }
+}
