a44d9c1e2d24089fb22299e611d5fb287e1bb3fd
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-15 19:56:44 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Avanced - Lab/08.Cinema Ticket/CinemaTicket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.Cinema_Ticket
+{
+    internal class CinemaTicket
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            double ticketPrice = 0.0;
+
+            switch (day)
+            {
+                case "Monday":
+                    ticketPrice = 12;
+                    break;
+                case "Tuesday":
+                    ticketPrice = 12;
+                    break;
+                case "Wednesday":
+                    ticketPrice = 14;
+                    break;
+                case "Thursday":
+                    ticketPrice = 14;
+                    break;
+                case "Friday":
+                    ticketPrice = 12;
+                    break;
+                case "Saturday":
+                    ticketPrice = 16;
+                    break;
+                case "Sunday":
+                    ticketPrice = 16;
+                    break;
+            }
+            Console.WriteLine(ticketPrice);
+        }
+    }
+}
