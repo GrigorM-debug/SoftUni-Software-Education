5d50b2a76772fc17079df813bc10864d647015b2
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-15 19:55:08 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Avanced - Lab/03. Animal Type/AnimalType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Xml;
+
+namespace _03._Animal_Type
+{
+    internal class AnimalType
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                break;
+                case "snake":
+                case "crocodile":
+                case "tortoise":
+                    Console.WriteLine("reptile");
+                break;
+                default:
+                    Console.WriteLine("unknown");
+                break;
+            }
+        }
+    }
+}
