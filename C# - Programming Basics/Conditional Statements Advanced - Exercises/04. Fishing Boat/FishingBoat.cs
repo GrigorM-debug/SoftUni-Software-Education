fac77efd378ecabab944b1382c7dec5704ff2c83
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-19 14:57:03 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Advanced - Exercises/04. Fishing Boat/FishingBoat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _04._Fishing_Boat
+{
+    internal class FishingBoat
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int crewBudget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishmanNumber = int.Parse(Console.ReadLine());
+            double shipPrice = 0.0;
+
+            switch (season)
+            {
+                case "Spring":
+                    shipPrice = 3000;
+                    break;
+                case "Summer":
+                    shipPrice = 4200;
+                    break;
+                case "Autumn":
+                    shipPrice = 4200;
+                    break;
+                case "Winter":
+                    shipPrice = 2600;
+                    break;
+            }
+            if (fishmanNumber <= 6)
+            {
+                shipPrice = shipPrice - (shipPrice * 0.10);
+            }
+            else if (fishmanNumber >= 7 && fishmanNumber <= 11)
+            {
+                shipPrice = shipPrice - (shipPrice * 0.15);
+            }
+            else if (fishmanNumber >= 12)
+            {
+                shipPrice = shipPrice - (shipPrice * 0.25);
+            }
+            if (fishmanNumber % 2 == 0 && season != "Autumn")
+            {
+                shipPrice = shipPrice - (shipPrice * 0.05);
+            }
+
+            double moneyLeft = 0;
+            double moneyNeeded = 0;
+            if (crewBudget >= shipPrice)
+            {
+                moneyLeft = crewBudget - shipPrice;
+                Console.WriteLine($"Yes! You have {moneyLeft:f2} leva left.");
+            }
+            else if (shipPrice > crewBudget)
+            {
+                moneyNeeded = shipPrice - crewBudget;
+                Console.WriteLine($"Not enough money! You need {moneyNeeded:f2} leva.");
+            }
+        }
+    }
+}
---
+++ C# - Programming Basics/Conditional Statements Advanced - Exercises/05. Journey/Journey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace _05._Journey
+{
+    internal class Journey
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string location = "";
+            string breakType = "";
+            double tripSum = 0.0;
+
+            if (budget <= 100)
+            {
+                location = "Bulgaria";
+                if (season == "summer")
+                {
+                    breakType = "Camp";
+                    tripSum = budget * 0.3;
+                }
+                else if (season == "winter")
+                {
+                    breakType = "Hotel";
+                    tripSum = budget * 0.7;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                location = "Balkans";
+                if(season == "summer")
+                {
+                    breakType = "Camp";
+                    tripSum = budget * 0.4;
+                }
+                else if (season == "winter")
+                {
+                    breakType = "Hotel";
+                    tripSum = budget * 0.8;
+                }
+            }
+            else if (budget > 1000)
+            {
+                location = "Europe";
+                tripSum = budget * 0.9;
+            }
+            Console.WriteLine($"Somewhere in {location}");
+            Console.WriteLine($"{breakType} - {tripSum:f2}");
+        }
+    }
+}
