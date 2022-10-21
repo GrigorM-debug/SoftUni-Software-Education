ec6de199161a3a8d0d782e52ce84ef543595a990
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-21 16:55:47 +0300

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Advanced - Exercises/06. Operations Between Numbers/OperationBetweenNumbers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Xml;
+
+namespace _06._Operations_Between_Numbers
+{
+    internal class OperationBetweenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double result = 0.0;
+            switch (operation)
+            {
+                case "+":
+                    result = n1 + n2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{n1} {operation} {n2} = {result} - even");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{n1} {operation} {n2} = {result} - odd");
+                    }
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{n1} {operation} {n2} = {result} - even");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{n1} {operation} {n2} = {result} - odd");
+                    }
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{n1} {operation} {n2} = {result} - even");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{n1} {operation} {n2} = {result} - odd");
+                    }
+                    break;
+
+                case "/":
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                    }
+                    else
+                    {
+                        result = 1.0 * n1 / n2;
+                        Console.WriteLine($"{n1} {operation} {n2} = {result:f2}");
+                    }
+                    break;
+                case "%":
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                    }
+                    else
+                    {
+                        result = 1.0 * n1 % n2;
+                        Console.WriteLine($"{n1} {operation} {n2} = {result}");
+                    }
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ C# - Programming Basics/Conditional Statements Advanced - Exercises/07. Hotel Room/HotelRoom.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _07._Hotel_Room
+{
+    internal class HotelRoom
+    {
+        static void Main(string[] args)
+        {
+            string mounth = Console.ReadLine();
+            int overnightStaysCnt = int.Parse(Console.ReadLine());
+            decimal studioPrice = 0;
+            decimal studioDiscount = 0.00M;
+            decimal apartmentPrice = 0.00M;
+            decimal apartmentDiscount = 0.10M;
+
+            switch (mounth)
+            {
+                case "May":
+                case "October":
+                    if (overnightStaysCnt <= 7)
+                    {
+                        studioPrice = 50M * overnightStaysCnt;
+                        apartmentPrice = 65M* overnightStaysCnt;
+                    }
+                    else if (overnightStaysCnt > 7 && overnightStaysCnt <= 14)
+                    {
+                        studioPrice = 50M * overnightStaysCnt;
+                        apartmentPrice = 65M * overnightStaysCnt;
+                        studioDiscount = 0.05M;
+                        studioPrice = studioPrice - (studioPrice * studioDiscount);
+                    }   
+                    else if (overnightStaysCnt > 14)
+                    {
+                        studioPrice = 50M * overnightStaysCnt;
+                        studioDiscount = 0.30M;
+                        studioPrice = studioPrice - (studioPrice * studioDiscount);
+                        apartmentPrice = 65M * overnightStaysCnt;
+                        apartmentPrice = apartmentPrice - (apartmentPrice * apartmentDiscount);
+                    }   
+                    break;
+                case "June":
+                case "September":
+                    if (overnightStaysCnt <= 7)
+                    {
+                        studioPrice = 75.20M * overnightStaysCnt;
+                        apartmentPrice = 68.70M * overnightStaysCnt;
+                    }
+                    else if (overnightStaysCnt > 7 && overnightStaysCnt <= 14)
+                    {
+                        studioPrice = 75.20M * overnightStaysCnt;
+                        apartmentPrice = 68.70M * overnightStaysCnt;
+                    }
+                    else if (overnightStaysCnt > 14)
+                    {
+                        studioPrice = 75.20M * overnightStaysCnt;
+                        apartmentPrice = 68.70M * overnightStaysCnt;
+                        studioDiscount = 0.20M;
+                        studioPrice = studioPrice - (studioPrice * studioDiscount);
+                        apartmentPrice = apartmentPrice - (apartmentPrice * apartmentDiscount);
+                    }
+                    break;
+                case "July":
+                case "August":
+                    if (overnightStaysCnt <= 7)
+                    {
+                        studioPrice = 76M * overnightStaysCnt;
+                        apartmentPrice = 77M * overnightStaysCnt;
+                    }
+                    else if (overnightStaysCnt > 7 && overnightStaysCnt <= 14)
+                    {
+                        studioPrice = 76M * overnightStaysCnt;
+                        apartmentPrice = 77M * overnightStaysCnt;
+                    }
+                    else if (overnightStaysCnt > 14)
+                    {
+                        studioPrice = 76M * overnightStaysCnt;
+                        apartmentPrice = 77M * overnightStaysCnt;
+                        apartmentPrice = apartmentPrice - (apartmentPrice * apartmentDiscount);
+                    }
+                    break;
+            }
+            Console.WriteLine($"Apartment: {apartmentPrice:F2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:F2} lv.");
+        }
+    }
+}
