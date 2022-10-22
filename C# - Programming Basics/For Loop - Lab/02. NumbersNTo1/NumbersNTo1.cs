68b0029c286c18d80a97028afca4eeb3ff153a83
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-22 16:11:49 +0300

    Add files via upload

---
+++ C# - Programming Basics/For Loop - Lab/02. NumbersNTo1/NumbersNTo1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._NumbersNTo1
+{
+    internal class NumbersNTo1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ C# - Programming Basics/For Loop - Lab/03. Numbers1ToNWithStep 3/Numbers1ToNWithStep3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Numbers1ToNWithStep_3
+{
+    internal class Numbers1ToNWithStep3
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
