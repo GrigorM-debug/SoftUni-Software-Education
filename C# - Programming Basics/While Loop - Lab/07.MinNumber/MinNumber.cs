415bde004171ea96d6f1e1b931b8d861395ee6a8
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-01 17:19:34 +0200

    Add files via upload

---
+++ C# - Programming Basics/While Loop - Lab/07.MinNumber/MinNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    internal class MinNumber
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int minNumber = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int currectNumber = int.Parse(input);
+
+                if (currectNumber < minNumber)
+                {
+                    minNumber = currectNumber;  
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
---
+++ C# - Programming Basics/While Loop - Lab/08.Graduation/Graduation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08.Graduation
+{
+    internal class Graduation
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currectClas = 1;
+            double allGrades = 0;
+            int badGrades = 0;
+
+            while (currectClas <= 12)
+            {
+                double currectMark = double.Parse(Console.ReadLine());
+                
+                if (currectMark >= 4)
+                {
+                    currectClas++;
+                    allGrades += currectMark;
+                }
+                else
+                {
+                    badGrades++;
+                    if (badGrades > 1)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {currectClas} grade");
+                        return;
+                    }
+                }
+            }
+            double averageGrade = allGrades / 12;
+            Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+        }
+    }
+}
