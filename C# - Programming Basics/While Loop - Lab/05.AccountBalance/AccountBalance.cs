28b53aa10bbf79a8d81802ecbc3579f18b0341fc
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-01 17:17:57 +0200

    Add files via upload

---
+++ C# - Programming Basics/While Loop - Lab/05.AccountBalance/AccountBalance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    internal class AccountBalance
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            double sum = 0;
+
+            while (n != "NoMoreMoney")
+            {
+                double amount = double.Parse(n);
+                if (amount >= 0)
+                {
+                    sum = sum + amount;
+                    Console.WriteLine($"Increase: {amount:f2}");
+                    n = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+            }
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
---
+++ C# - Programming Basics/While Loop - Lab/06.MaxNumber/MaxNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    internal class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int maxNumber = int.MinValue;
+            
+            while(number != "Stop")
+            {
+                int currectNumber = int.Parse(number);
+                if (currectNumber > maxNumber)
+                {
+                    maxNumber = currectNumber;
+                }
+                number = Console.ReadLine();
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
