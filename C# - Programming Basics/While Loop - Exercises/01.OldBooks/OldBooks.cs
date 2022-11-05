f22deea5fe55984d9b2b7a7a598838c97a322360
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-05 17:26:24 +0200

    Add files via upload

---
+++ C# - Programming Basics/While Loop - Exercises/01.OldBooks/OldBooks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //запазваме любимата книга на Ани 
+            string favouriteBook = Console.ReadLine();
+
+            int count = 0; //броя книги, които сме прегледали в търсене на любимата
+            string input = Console.ReadLine();
+
+            //създаваме цикъл, в който всеки път ще четем заглавието на нова книга
+            //   => докато не минем през всички книги (докато не получим: "No More Books".)
+            //   => докато не намерим любимата книга 
+
+            while (input != "No More Books")
+            {
+                if (input == favouriteBook)
+                {
+                    //Ако сме намерили книгата:
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    break;
+                }
+
+                count++;
+                input = Console.ReadLine();
+            }
+
+            //Ако не сме намерили книгата: 
+            if (input == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");
+            }
+
+
+
+        }
+    }
+}
---
+++ C# - Programming Basics/While Loop - Exercises/03. Vacation/Vacation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    internal class Vacation
+    {
+        static void Main(string[] args)
+        {
+            double holidayPrice = double.Parse(Console.ReadLine());
+            double availableMoney = double.Parse(Console.ReadLine());
+            double money = 0;
+            string input = "";
+            int DaysCounter = 0;
+            int spendingMoneyCounter = 0;
+
+            while(availableMoney<holidayPrice)
+            {
+                input = Console.ReadLine();
+                money = double.Parse(Console.ReadLine());
+                DaysCounter++;
+                
+                if (input == "save")
+                {
+                    availableMoney += money;
+                    spendingMoneyCounter = 0;
+                }
+                else
+                {
+                    availableMoney -= money;
+                    if (availableMoney < 0)
+                    {
+                        availableMoney = 0;
+                    }
+
+                    spendingMoneyCounter++;
+
+                    if (spendingMoneyCounter == 5)
+                    {
+                        Console.WriteLine($"You can't save the money.");
+                        Console.WriteLine($"{DaysCounter}");
+                        break;
+                    }
+                }
+            
+            }
+            if (availableMoney >= holidayPrice)
+            {
+                Console.WriteLine($"You saved the money for {DaysCounter} days.");
+            }
+        }
+    }
+}
