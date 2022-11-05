36a1a0ce1a5b4392a49674ba310ed54c9fd5a13a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-05 17:27:22 +0200

    Add files via upload

---
+++ C# - Programming Basics/While Loop - Exercises/04.Walking/Walking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04.Walking
+{
+    internal class Walking
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int stepsGoal = 10000;
+            int steps = 0;
+
+            while (input != "Going home")
+            {
+                steps += int.Parse(input);
+
+                if (steps >= 10000)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "Going home")
+            {
+                input = Console.ReadLine();
+                steps += int.Parse(input);
+            }
+
+            if (steps < stepsGoal)
+            {
+                int stepsNeeded = stepsGoal - steps;
+                Console.WriteLine($"{stepsNeeded} more steps to reach goal.");
+            }
+            else
+            {
+                int stepsOver = steps - stepsGoal;
+                Console.WriteLine($"Goal reached! Good job!");
+                Console.WriteLine($"{stepsOver} steps over the goal!");
+            }
+        }
+    }
+}
