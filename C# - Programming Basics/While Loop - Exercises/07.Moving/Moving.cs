4379cf1c2a92695cd779f73d2a9d3485ca409e78
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-06 17:05:27 +0200

    Add files via upload

---
+++ C# - Programming Basics/While Loop - Exercises/07.Moving/Moving.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07.Moving
+{
+    internal class Moving
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());  
+            int h = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int cubicMeters = 0;
+            int leftSpace = w * l * h;
+
+            while (input != "Done")
+            {
+                cubicMeters += int.Parse(input);
+
+                if (cubicMeters > leftSpace)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            int diff = leftSpace - cubicMeters;
+
+            if (diff >= 0)
+            {
+                Console.WriteLine($"{diff} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(diff)} Cubic meters more.");
+            }
+        }
+    }
+}
