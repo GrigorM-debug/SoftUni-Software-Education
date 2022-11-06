9c80a26ab4f474e20daf5f970ffb8b658c9c6f1b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-06 16:54:25 +0200

    Add files via upload

---
+++ C# - Programming Basics/While Loop - Exercises/06.Cake/Cake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeHeight = int.Parse(Console.ReadLine());
+            int cakeSize = cakeWidth * cakeHeight;
+            int cakePieces = 0;
+            string input = Console.ReadLine();
+
+
+            while (input != "STOP")
+            {
+                cakePieces += int.Parse(input);
+
+                if (cakePieces>cakeSize)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            int diff = cakeSize - cakePieces;
+
+            if (input == "STOP" && diff >= 0)
+            {
+                Console.WriteLine($"{diff} pieces are left.");    
+            }
+            else
+            {
+                 Console.WriteLine($"No more cake left! You need {Math.Abs(diff)} pieces more.");
+            }
+        }
+    }
+}
