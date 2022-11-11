75512569720779803b68af080b01f45d6b944581
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-11 14:27:07 +0200

    Add files via upload

---
+++ C# - Programming Basics/Nested Loops - Lab/06.Building/Building.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06.Building
+{
+    internal class Building
+    {
+        static void Main(string[] args)
+        {
+            int floor = int.Parse(Console.ReadLine());
+            int roomsCnt = int.Parse(Console.ReadLine());
+
+            for (int i = floor; i >= 1; i--)
+            {
+                for (int j = 0; j < roomsCnt; j++)
+                {
+                    if (i == floor)
+                    {
+                        Console.Write($"L{i}{j} ");
+                    }
+                    else if (i % 2 != 0)
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                    else if (i % 2 ==0)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                }
+                Console.WriteLine(); //When rooms on floor are equal to roomsCnt we tell the program to go on new line for other rooms. 
+
+            }
+           
+        }
+    }
+}
