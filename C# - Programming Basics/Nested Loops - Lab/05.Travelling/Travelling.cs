dd45c0fcf0ef63935cade12502dc726cf4ae228d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-11 13:49:28 +0200

    Add files via upload

---
+++ C# - Programming Basics/Nested Loops - Lab/05.Travelling/Travelling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    internal class Travelling
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            double budget = 0;
+            while(destination != "End")
+            {
+                budget = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+
+                while(savedMoney<budget)
+                {
+                    double currentMoney = double.Parse(Console.ReadLine());
+                    savedMoney = savedMoney + currentMoney;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
