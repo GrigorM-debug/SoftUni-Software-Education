925e4357eaebe2ffa34402ee292a2f9740a62c2f
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-12 18:23:04 +0200

    Add files via upload

---
+++ C# - Programming Basics/Exam Preparation/01.Oscars ceremony/OscarsCeremony.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Oscars_ceremony
+{
+    internal class OscarCeremony
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+
+            double statuettePrice = rent - rent * 0.30;
+            double cateringPrice = statuettePrice - statuettePrice * 0.15;
+            double soundingPrice = cateringPrice / 2;
+            double totalPrice = rent + statuettePrice + cateringPrice + soundingPrice;
+
+            Console.WriteLine($"{totalPrice:f2}");  
+        }
+    }
+}
---
+++ C# - Programming Basics/Exam Preparation/02.MovieDay/MovieDay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Transactions;
+
+namespace _02.MovieDay
+{
+    internal class MovieDay
+    {
+        static void Main(string[] args)
+        {
+            int photoTime = int.Parse(Console.ReadLine());
+            int sceneCnt = int.Parse(Console.ReadLine());
+            int sceneTime = int.Parse(Console.ReadLine());
+
+            double timeForPreparation = photoTime * 0.15;
+            double timeForSceneFilming = sceneCnt * sceneTime;
+            double totalTime = timeForPreparation + timeForSceneFilming;
+
+            if(totalTime <= photoTime)
+            {
+                double timeLeft = photoTime - totalTime;
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeLeft)} minutes left!");
+            }
+            else
+            {
+                double timeNeeded = totalTime - photoTime;
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(timeNeeded)} minutes.");
+            }
+           
+        }
+    }
+}
