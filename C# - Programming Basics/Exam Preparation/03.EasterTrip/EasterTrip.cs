53851fb34c40e14a913559a7031c5a89393507a8
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-12 18:24:18 +0200

    Add files via upload

---
+++ C# - Programming Basics/Exam Preparation/03.EasterTrip/EasterTrip.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _03.EasterTrip
+{
+    internal class EasterTrip
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            string holidayDate = Console.ReadLine();
+            int overnightsCnt = int.Parse(Console.ReadLine());
+            int overnightsPrice = 0;
+
+            switch (destination)
+            {
+                case "France":
+                    switch (holidayDate)
+                    {
+                        case "21-23":
+                            overnightsPrice = 30;
+                            break;
+                        case "24-27":
+                            overnightsPrice = 35;
+                            break;
+                        case "28-31":
+                            overnightsPrice = 40;
+                            break;
+                    }
+                    break;
+                case "Italy":
+                    switch (holidayDate)
+                    {
+                        case "21-23":
+                            overnightsPrice = 28;
+                            break;
+                        case "24-27":
+                            overnightsPrice = 32;
+                            break;
+                        case "28-31":
+                            overnightsPrice = 39;
+                            break;
+                    }
+                    break;
+                case "Germany":
+                    switch (holidayDate)
+                    {
+                        case "21-23":
+                            overnightsPrice = 32;
+                            break;
+                        case "24-27":
+                            overnightsPrice = 37;
+                            break;
+                        case "28-31":
+                            overnightsPrice = 43;
+                            break;
+                    }
+                    break;
+            }
+            int totalPrice = overnightsCnt * overnightsPrice;
+            Console.WriteLine($"Easter trip to {destination} : {totalPrice:f2} leva.");
+        }
+    }
+}
---
+++ C# - Programming Basics/Exam Preparation/04.EasterEggs/EasterEggs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _04.EasterEggs
+{
+    internal class EasterEggs
+    {
+        static void Main(string[] args)
+        {
+            int eggsCnt = int.Parse(Console.ReadLine());
+
+            int redEggsCounter = 0;
+            int orangeEggsCounter = 0;
+            int blueEggsCounter = 0;
+            int greenEggsCounter = 0;
+
+            int maxColorCnt = int.MinValue;
+            string maxColor = "";
+
+            for (int i = 0; i <= eggsCnt; i++)
+            {
+                string eggsColor = Console.ReadLine();
+
+                switch (eggsColor)
+                {
+                    case "red":
+                        redEggsCounter++;
+                        if (redEggsCounter > maxColorCnt)
+                        {
+                            maxColorCnt = redEggsCounter;
+                            maxColor = eggsColor;
+                        }
+                        break;
+                    case "orange":
+                        orangeEggsCounter++;
+                        if (orangeEggsCounter > maxColorCnt)
+                        {
+                            maxColorCnt = orangeEggsCounter;
+                            maxColor = eggsColor;
+                        }
+                        break;
+                    case "blue":
+                        blueEggsCounter++;
+                        if (blueEggsCounter > maxColorCnt)
+                        {
+                            maxColorCnt = blueEggsCounter;
+                            maxColor = eggsColor;
+                        }
+                        break;
+                    case "green":
+                        greenEggsCounter++;
+                        if (greenEggsCounter > maxColorCnt)
+                        {
+                            maxColorCnt = greenEggsCounter;
+                            maxColor = eggsColor;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine($"Red eggs: {redEggsCounter}");
+            Console.WriteLine($"Orange eggs: {orangeEggsCounter}");
+            Console.WriteLine($"Blue eggs: {blueEggsCounter}");
+            Console.WriteLine($"Green eggs: {greenEggsCounter}");
+            Console.WriteLine($"Max eggs: {maxColorCnt} -> {maxColor}");
+        }
+    }
+}
