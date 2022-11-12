332926369b66155722546fb501ad8d7e6d8f9aac
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-13 01:28:07 +0200

    Add files via upload

---
+++ C# - Programming Basics/Exam Preparation/06.EasterDecoration/EasterDecoration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _06.EasterDecoration
+{
+    internal class EasterDecoration
+    {
+        static void Main(string[] args)
+        {
+            int customersCnt = int.Parse(Console.ReadLine());
+            double allPurchase = 0.0;
+
+            for (int i = 0; i < customersCnt; i++)
+            {
+                string product = Console.ReadLine();
+                double currentClientPurchase = 0;
+                int purchaseCnt = 0;
+
+                while (product != "Finish")
+                {
+                    if (product == "basket")
+                    {
+                        currentClientPurchase = currentClientPurchase + 1.50;
+                        purchaseCnt++;
+                    }
+                    else if (product == "wreath")
+                    {
+                        currentClientPurchase = currentClientPurchase + 3.80;
+                        purchaseCnt++;
+                    }
+                    else if (product == "chocolate bunny")
+                    {
+                        currentClientPurchase = currentClientPurchase + 7;
+                        purchaseCnt++;
+                    }
+                    product = Console.ReadLine();
+                }
+                if (purchaseCnt % 2 == 0)
+                {
+                    currentClientPurchase = currentClientPurchase - currentClientPurchase * 0.20;
+                }
+
+                allPurchase += currentClientPurchase;
+                Console.WriteLine($"You purchased {purchaseCnt} items for {currentClientPurchase:f2} leva.");
+            }
+            double average = allPurchase / customersCnt;
+            Console.WriteLine($"Average bill per client is: {average:f2} leva.");
+        }
+    }
+}
