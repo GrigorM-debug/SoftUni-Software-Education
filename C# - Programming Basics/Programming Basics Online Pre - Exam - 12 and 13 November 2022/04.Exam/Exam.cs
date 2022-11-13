5ab194f8125ca15d6e42a0c48d477036e7d6be43
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-11-13 18:14:20 +0200

    Add files via upload

---
+++ C# - Programming Basics/Programming Basics Online Pre - Exam - 12 and 13 November 2022/04.Exam/Exam.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _04.Exam
+{
+    internal class Exam
+    {
+        static void Main(string[] args)
+        {
+            int studentsCnt = int.Parse(Console.ReadLine());
+            int groupOne = 0;
+            int groupTwo = 0;
+            int groupThree = 0;
+            int groupFour = 0;
+
+            
+            double averageGrade = 0;
+
+            for (int i = 1; i <= studentsCnt; i++)
+            {
+                double mark = double.Parse(Console.ReadLine());
+                
+                if (mark < 3)
+                {
+                    groupFour++;
+                    averageGrade += mark;
+                }
+                else if (mark >= 3 && mark <= 3.99)
+                {
+                    groupThree++;
+                    averageGrade += mark;
+                }
+                else if (mark >= 4 && mark <= 4.99)
+                {
+                    groupTwo++;
+                    averageGrade += mark;
+                }
+                else if (mark >= 5)
+                {
+                    groupOne++;
+                    averageGrade += mark;
+                }
+
+                //double groupOnePercent = groupOne / studentsCnt * 100;
+                //double groupTwoPercent = groupTwo / studentsCnt * 100;
+                //double groupThreePercent = groupThree / studentsCnt * 100;
+                //double groupFourPercent = groupFour / studentsCnt * 100;
+
+                //average = sum / studentsCnt;
+            }
+            double groupOnePercent = (groupOne / (studentsCnt * 1.0)) * 100;
+            double groupTwoPercent = (groupTwo / (studentsCnt * 1.0)) * 100;
+            double groupThreePercent = (groupThree / (studentsCnt * 1.0)) * 100;
+            double groupFourPercent = (groupFour / (studentsCnt * 1.0)) * 100;
+
+            averageGrade = averageGrade / studentsCnt;
+            Console.WriteLine($"Top students: {groupOne:f2}%");
+            Console.WriteLine($"Between 4.00 and 4.99: {groupTwo:f2}%");
+            Console.WriteLine($"Between 3.00 and 3.99: {groupThree:f2}%");
+            Console.WriteLine($"Fail: {groupFour:f2}%");
+            Console.WriteLine($"Average: {averageGrade:f2}");
+        }
+    }
+}
---
+++ C# - Programming Basics/Programming Basics Online Pre - Exam - 12 and 13 November 2022/05.ExcursionSale/ExcursionSale.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _05.ExcursionSale
+{
+    internal class ExcursionSale
+    {
+        static void Main(string[] args)
+        {
+            int seaHolidaysCnt = int.Parse(Console.ReadLine());
+            int mountainHolidaysCnt = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            double price = 0;
+            
+
+            while (input != "Stop")
+            {
+                input = Console.ReadLine();
+
+                if (input == "Stop")
+                {
+                    break;
+                }
+                if (input == "sea")
+                {
+                    if (seaHolidaysCnt == 0)
+                    {
+                        continue;
+                    }
+                    price = price + 680;
+                    seaHolidaysCnt -= 1;
+                }
+                if (input == "mountain")
+                {
+                    if (mountainHolidaysCnt == 0)
+                    {
+                        continue;
+                    }
+                    price = price + 499;
+                    mountainHolidaysCnt -= 1;
+                }
+
+                if (seaHolidaysCnt == 0 && mountainHolidaysCnt == 0)
+                {
+                    Console.WriteLine("Good job! Everything is sold.");
+                    break;
+                }
+                
+            }
+
+
+            Console.WriteLine($"Profit: {price} leva.");
+            
+
+
+        }
+    }
+}
