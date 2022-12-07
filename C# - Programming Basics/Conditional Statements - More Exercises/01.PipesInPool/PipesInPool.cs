4f0b95abb63368d4be17531c4aa0d176d9bfddc2
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-12-07 15:40:24 +0200

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements - More Exercises/01.PipesInPool/PipesInPool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _01.PipesInPool
+{
+    internal class PipesInPool
+    {
+        static void Main(string[] args)
+        {
+            double volume = double.Parse(Console.ReadLine());
+            double p1 = double.Parse(Console.ReadLine()); 
+            double p2 = double.Parse(Console.ReadLine()); 
+            double h = double.Parse(Console.ReadLine());
+
+            double totalLitters = p1 * h + p2 * h; 
+
+            if (totalLitters <= volume)
+            {
+                double totalLittersInPercent = totalLitters / volume * 100;
+                double p1Percent = p1 * h / totalLitters * 100;
+                double p2Percent = p2 * h / totalLitters * 100;
+                Console.WriteLine($"The poool is {totalLittersInPercent:F2}% full. Pipe 1 : {p1Percent:F2}%. Pipe 2: {p2Percent:F2}% liters.");
+            }
+            else if (totalLitters > volume)
+            {
+                totalLitters = totalLitters - volume;   
+                Console.WriteLine($"For {h:F2} hours the pool overflows with {totalLitters:F2} liters.");
+            }
+        }
+    }
+}
---
+++ C# - Programming Basics/Conditional Statements - More Exercises/02.SleepyTomCat/SleepyCatTom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.SleepyTomCat
+{
+    internal class SleepyCatTom
+    {
+        static void Main(string[] args)
+        {
+            int restDays = int.Parse(Console.ReadLine());
+
+            int tomYearPlayingTime = 30000;
+
+            int workingDays = 365 - restDays;
+            int timePlaying = workingDays * 63 + restDays * 127;
+
+            if (timePlaying > tomYearPlayingTime)
+            {
+                int difference = timePlaying- tomYearPlayingTime;
+                int differenceInHours = difference / 60;
+                differenceInHours = Math.Abs(differenceInHours);
+                int differenceInMinutes = difference%60;
+                differenceInMinutes= Math.Abs(differenceInMinutes); 
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{differenceInHours} hours and {differenceInMinutes} minutes more for play");
+            }
+            else if (timePlaying < tomYearPlayingTime)
+            {
+                int difference = tomYearPlayingTime - timePlaying;  
+                int differenceInHours = difference / 60;
+                differenceInHours = Math.Abs(differenceInHours);
+                int differenceInMinutes = difference%60;
+                differenceInMinutes = Math.Abs(differenceInMinutes);
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{differenceInHours} hours and {differenceInMinutes} minutes less for play");
+            }
+
+        }
+    }
+}
