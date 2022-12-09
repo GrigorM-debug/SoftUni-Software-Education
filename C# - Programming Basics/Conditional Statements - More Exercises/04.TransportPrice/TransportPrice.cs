8c3a76c6e1aeffca13d09496e96bc8fe9ba59aba
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-12-09 17:02:52 +0200

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements - More Exercises/04.TransportPrice/TransportPrice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace _04.TransportPrice
+{
+    internal class TransportPrice
+    {
+        static void Main(string[] args)
+        {
+            int kilometersCnt = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            double price = 0.0;
+
+            if(kilometersCnt <= 20)
+            {
+                switch (timeOfDay)
+                {
+                    case "day":
+                        price = 0.70 + kilometersCnt * 0.79;
+                        break;
+                    case "night":
+                        price = 0.70 + kilometersCnt * 0.90;
+                        break;
+                }
+            }
+            if (kilometersCnt >= 20 && kilometersCnt < 100) 
+            {
+                price = kilometersCnt * 0.09;
+            }
+            if (kilometersCnt >= 100)
+            {
+                price = kilometersCnt * 0.06; 
+            }
+
+            Console.WriteLine("{0:F2}", price);
+
+        }
+    }
+}
