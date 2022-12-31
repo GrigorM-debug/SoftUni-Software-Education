8f2bfa6a0c782545359f405c817154d170113e63
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-01 01:19:01 +0200

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Advanced - More Exercises/10.MultiplyBy2/MultiplyBy2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10.MultiplyBy2
+{
+    internal class MultiplyBy2
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            while (num >= 0)
+            {
+                result = num * 2;
+                Console.WriteLine($"Result: {result:f2}");
+                num = double.Parse(Console.ReadLine());
+            }
+            if (num < 0)
+            {
+                Console.WriteLine("Negative number!");
+            }
+        }
+    }
+}
