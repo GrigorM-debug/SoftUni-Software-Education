0e319a8d88aa502e1f3ae86c1f0c09c5ebf71907
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-01 00:45:02 +0200

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Advanced - More Exercises/05.Vacation/Vacation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace _05.Vacation
+{
+    internal class Vacation
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string place = "";
+            string location = "";
+
+            if (budget <= 1000)
+            {
+                place = "Camp";
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        budget = budget * 0.65;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        budget = budget * 0.45;
+                        break;
+                }
+            }
+            else if (budget > 1000 && budget <= 3000)
+            {
+                place = "Hut";
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        budget = budget * 0.80;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        budget = budget * 0.60;
+                        break;
+                }
+            }
+            else
+            {
+                place = "Hotel";
+                switch(season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        budget = budget * 0.90;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        budget = budget * 0.90;
+                        break;
+                }
+            }
+            Console.WriteLine($"{location} - {place} - {budget:f2}");
+        }
+    }
+}
