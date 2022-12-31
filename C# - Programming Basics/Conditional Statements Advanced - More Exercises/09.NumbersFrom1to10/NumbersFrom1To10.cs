e27373d6f53dd8f970c6eee87e3aeb11cdb9bd02
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-01 01:07:24 +0200

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Advanced - More Exercises/09.NumbersFrom1to10/NumbersFrom1To10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09.NumbersFrom1to10
+{
+    internal class NumbersFrom1To10
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i<=10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
