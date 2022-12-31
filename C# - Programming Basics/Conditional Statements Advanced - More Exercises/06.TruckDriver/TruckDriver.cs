30e2becfb61bc7ec0bb864e11b6806345a75a372
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-01 01:01:27 +0200

    Add files via upload

---
+++ C# - Programming Basics/Conditional Statements Advanced - More Exercises/06.TruckDriver/TruckDriver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _06.TruckDriver
+{
+    internal class TruckDriver
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double kilometersPerMouth = double.Parse(Console.ReadLine());
+            double money = 0.0;
+
+            if (kilometersPerMouth <= 5000)
+            {
+                switch (season)
+                {
+                    case "Summer":
+                        money = kilometersPerMouth * 0.90 * 4;
+                        break;
+                    case "Autumn":
+                        money = kilometersPerMouth * 0.75 * 4;
+                        break;
+                    case "Spring":
+                        money = kilometersPerMouth * 0.75 * 4;
+                        break;
+                    case "Winter":
+                        money = kilometersPerMouth * 1.05 * 4;
+                        break;
+                }
+            }
+            else if (kilometersPerMouth > 5000 && kilometersPerMouth <= 10000)
+            {
+                switch (season)
+                {
+                    case "Summer":
+                        money = kilometersPerMouth * 1.10 * 4;
+                        break;
+                    case "Autumn":
+                        money = kilometersPerMouth * 0.95 * 4;
+                        break;
+                    case "Spring":
+                        money = kilometersPerMouth * 0.95 * 4;
+                        break;
+                    case "Winter":
+                        money = kilometersPerMouth * 1.25 * 4;
+                        break;
+                }
+            }
+            else
+            {
+                switch (season)
+                {
+                    case "Summer":
+                        money = kilometersPerMouth * 1.45 * 4;
+                        break;
+                    case "Autumn":
+                        money = kilometersPerMouth * 1.45 * 4;
+                        break;
+                    case "Spring":
+                        money = kilometersPerMouth * 1.45 * 4;
+                        break;
+                    case "Winter":
+                        money = kilometersPerMouth * 1.45 * 4;
+                        break;
+                }
+            }
+            money = money - money * 0.10;
+            Console.WriteLine($"{money:f2}");   
+        }
+    }
+}
