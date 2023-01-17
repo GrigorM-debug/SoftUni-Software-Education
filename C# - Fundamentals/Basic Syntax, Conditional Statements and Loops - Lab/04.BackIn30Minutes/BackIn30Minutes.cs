75e43e8aa2e2499b1438737018ffd8c290775beb
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-17 18:25:20 +0200

    Add files via upload

---
+++ C# - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/04.BackIn30Minutes/BackIn30Minutes.cs
@@ -0,0 +1,5 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+var time = new TimeSpan(hours, minutes + 30, 0);
+Console.WriteLine($"{time:h\\:mm}");
---
+++ C# - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/09.SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,19 @@
+﻿int inputCnt = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int counter = 0;
+
+for (int i=1; i<=100; i+=2)
+{
+    if (i %2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+    sum = sum + i;
+    counter++;
+    if (counter == inputCnt)
+    {
+        Console.WriteLine($"Sum: {sum}");
+        break;
+    }
+}
