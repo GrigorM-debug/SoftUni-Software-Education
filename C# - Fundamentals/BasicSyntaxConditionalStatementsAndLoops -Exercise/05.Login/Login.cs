685419428f2a111e8feac1d394a97ca987bbbe7b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-18 16:46:45 +0200

    Added new task

---
+++ C# - Fundamentals/BasicSyntaxConditionalStatementsAndLoops -Exercise/05.Login/Login.cs
@@ -0,0 +1,25 @@
+﻿
+string userName = Console.ReadLine();
+string password = new string(userName.Reverse().ToArray());
+
+int failedAttemps = 0;
+
+while (true)
+{
+    string givedPassword = Console.ReadLine();
+    if (givedPassword == password)
+    {
+        Console.WriteLine($"User {userName} logged in.");
+        break;
+    }
+    else
+    {
+        failedAttemps++;    
+        Console.WriteLine($"Incorrect password. Try again.");
+    }
+    if (failedAttemps == 3)
+    {
+        Console.WriteLine($"User {userName} blocked!");
+        break;
+    }
+}
