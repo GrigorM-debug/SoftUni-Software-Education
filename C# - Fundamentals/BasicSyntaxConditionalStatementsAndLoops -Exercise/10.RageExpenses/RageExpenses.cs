135590321da7f244e25fcebc6764cb3fbd0b18e0
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-19 21:49:01 +0200

    Довърши тази 10та задача

---
+++ C# - Fundamentals/BasicSyntaxConditionalStatementsAndLoops -Exercise/10.RageExpenses/RageExpenses.cs
@@ -0,0 +1,21 @@
+﻿int lostGame = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());   
+double mousePrice = double.Parse(Console.ReadLine()); 
+double keyboardPrice = double.Parse(Console.ReadLine());  
+double displayPrice = double.Parse(Console.ReadLine());
+
+int trashedHeadset = 0;
+int trashedMouse = 0;
+int trashedKeyboard = 0;
+int trashedDisplay = 0;
+
+
+
+double keyboardTotalPrice = trashedKeyboard * keyboardPrice;
+double mouseTotalPrice = trashedMouse * mousePrice;
+double headsetTotalPrice = trashedHeadset * headsetPrice;
+double displayTotalPrice = trashedDisplay * displayPrice;
+
+double expenses = keyboardTotalPrice + mouseTotalPrice + headsetTotalPrice + displayTotalPrice;
+
+Console.WriteLine($"Rage expenses: {expenses:f2}lv.");
