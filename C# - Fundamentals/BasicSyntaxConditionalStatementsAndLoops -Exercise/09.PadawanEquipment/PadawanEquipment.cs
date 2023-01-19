3fb97843165447f5ab35df37c7810ce0e6408d0e
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-19 20:57:05 +0200

    Da ti eba maikata i zadacha

---
+++ C# - Fundamentals/BasicSyntaxConditionalStatementsAndLoops -Exercise/09.PadawanEquipment/PadawanEquipment.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Pipes;
+
+double JohnMoney = double.Parse(Console.ReadLine());
+int studentsCnt = int.Parse(Console.ReadLine());
+double priceForOneLightSaber = double.Parse(Console.ReadLine());  
+double priceForOneRobe = double.Parse(Console.ReadLine());  
+double priceForOneBelt = double.Parse(Console.ReadLine());
+
+int freeBelts = (studentsCnt / 6);
+
+//Calculating the prices for equipment
+double lightSabersPrice = priceForOneLightSaber * (Math.Ceiling(studentsCnt + studentsCnt * 0.10));
+double robesPrice = priceForOneRobe * (studentsCnt);
+double beltsPrice = priceForOneBelt * (studentsCnt - freeBelts);
+
+//Calculating the total price for all shits
+double totalPrice = lightSabersPrice + robesPrice + beltsPrice;
+
+if (totalPrice <= JohnMoney)
+{
+    Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+}
+else
+{
+    double neededMoney = totalPrice - JohnMoney;
+    Console.WriteLine($"John will need {neededMoney:f2}lv more.");
+}
