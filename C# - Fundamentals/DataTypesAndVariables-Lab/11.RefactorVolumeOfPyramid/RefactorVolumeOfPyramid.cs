4e243b48aa1dffdd47bf2e11000f6cf20c17f0ab
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 01:53:45 +0200

    refactoring working code from variables span

---
+++ C# - Fundamentals/DataTypesAndVariables-Lab/11.RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Length: ");
+double lenght = double.Parse(Console.ReadLine());
+
+Console.Write("Width: ");
+double width = double.Parse(Console.ReadLine());
+
+Console.Write("Height: ");
+double height = double.Parse(Console.ReadLine());  
+
+double volumeOfPiramid = (lenght * width * height) / 3;
+
+Console.WriteLine($"Pyramid Volume: {volumeOfPiramid:f2}");
