8ab63ccb00449e266758ac5be0c6746fcca0c9c5
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 01:24:59 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Lab/09.CharsToString/CharsToString.cs
@@ -0,0 +1,9 @@
+﻿char firstSymbol = char.Parse(Console.ReadLine());
+char secondSymbol = char.Parse(Console.ReadLine()); 
+char thirdSymbol = char.Parse(Console.ReadLine());  
+
+string firstSymbolInString = char.ToString(firstSymbol);
+string secondSymbolToString = char.ToString(secondSymbol);
+string thirdSymbolToString = char.ToString(thirdSymbol);
+
+Console.WriteLine(firstSymbolInString + secondSymbolToString + thirdSymbolToString);
