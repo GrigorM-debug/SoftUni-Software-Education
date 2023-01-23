f6cb1aa54a9f971e417f88fa032bc0f48e96ccbe
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 01:34:09 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Lab/06.ReversedChars/ReversedChars.cs
@@ -0,0 +1,9 @@
+﻿char firstSymbol = char.Parse(Console.ReadLine());
+char secondSymbol = char.Parse(Console.ReadLine());
+char thirdSymbol = char.Parse(Console.ReadLine());
+
+string firstSymbolToString = char.ToString(firstSymbol);
+string secondSymbolToString = char.ToString(secondSymbol);  
+string thirdSymbolToString = char.ToString(thirdSymbol);
+
+Console.WriteLine($"{thirdSymbolToString} {secondSymbolToString} {firstSymbolToString}");
