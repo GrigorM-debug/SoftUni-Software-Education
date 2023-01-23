6fc7a478288d4d8eca1f430c21b9962dc917a162
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-23 17:36:13 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Lab/05.SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,21 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<= number; i++)
+{
+    int currentNumber = i;
+    int sum = 0;
+
+    while (currentNumber > 0)
+    { 
+        sum += currentNumber % 10;
+        currentNumber = currentNumber / 10;
+    }
+    if (sum == 5 || sum == 7 || sum == 11)
+    {
+        Console.WriteLine($"{i} -> True");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> False");
+    }
+}
