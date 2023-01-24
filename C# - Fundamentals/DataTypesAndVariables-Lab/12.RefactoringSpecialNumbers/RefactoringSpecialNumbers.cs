a4550d99921caf4d4b53b2bac8dad807a455985f
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 02:07:44 +0200

    Refactoring working example code for task 05.SpecialNumber

---
+++ C# - Fundamentals/DataTypesAndVariables-Lab/12.RefactoringSpecialNumbers/RefactoringSpecialNumbers.cs
@@ -0,0 +1,25 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+bool IsSpecialNum= false;
+
+for (int i = 1; i <= number; i++)
+{
+    int currectNumber = i;
+    int sum = 0;
+
+    while (currectNumber > 0)
+    {
+        sum += currectNumber % 10;
+        currectNumber /= 10;
+    }
+    if((sum == 5) || (sum == 7) || (sum == 11))
+    {
+        IsSpecialNum= true;
+        Console.WriteLine("{0} -> {1}", i, IsSpecialNum);
+    }
+    else
+    {
+        IsSpecialNum = false;
+        Console.WriteLine("{0} -> {1}", i, IsSpecialNum);
+    }
+}
