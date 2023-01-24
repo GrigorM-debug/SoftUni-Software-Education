fe4739ee302c099d0c24a26d73a2a749219cd269
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 16:51:53 +0200

    new solution for new lection - DataTypesAndVariables-Exercises

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/01.IntegerOperations/Integersoperation.cs
@@ -0,0 +1,11 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());   
+int fourthNumber = int.Parse(Console.ReadLine());
+
+int sum = firstNumber + secondNumber; // addind first number to second number
+int divide = sum / thirdNumber; // dividing the suim of first two number by the third number
+int multiply = divide * fourthNumber; // multiply the result of second operation by the fourth number
+
+Console.WriteLine(multiply); // printing the result after the last operation
+
