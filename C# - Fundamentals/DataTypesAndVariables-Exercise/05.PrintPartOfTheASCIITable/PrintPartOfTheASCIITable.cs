0afce37c1cae46fef4b2e60e12ed35c23067eac1
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 17:11:54 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/05.PrintPartOfTheASCIITable/PrintPartOfTheASCIITable.cs
@@ -0,0 +1,9 @@
+﻿int startingPoint = int.Parse(Console.ReadLine());
+int endPoint = int.Parse(Console.ReadLine());   
+
+for (int i = startingPoint; i<= endPoint; i++)
+{
+    char symbols = (char)i;
+
+    Console.Write(symbols + " ");
+}
