bb03d3706291e2af23cfb539ea458c0586fd7b1f
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-24 17:04:21 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/04.SumOfChars/SumOfChars.cs
@@ -0,0 +1,9 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+for(int i = 1; i <= number; i++)
+{
+    char input = char.Parse(Console.ReadLine());
+    sum = sum + (int)input;
+}
+Console.WriteLine($"The sum equals: {sum}");
