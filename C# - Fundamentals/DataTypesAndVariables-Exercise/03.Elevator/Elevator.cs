91336d8c0ef6b25ab9f1d17dc9b75166bc6eb865
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-25 15:25:12 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/03.Elevator/Elevator.cs
@@ -0,0 +1,6 @@
+﻿int numberOfPeople = int.Parse(Console.ReadLine());
+int capasityP = int.Parse(Console.ReadLine());
+
+int courses = (int)Math.Ceiling((double)numberOfPeople / capasityP);
+
+Console.WriteLine(courses);
---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/07.WaterOverflow/WaterOverflow.cs
@@ -0,0 +1,19 @@
+﻿int waterTankCapasity = 255;
+int numberOfInput = int.Parse(Console.ReadLine());
+int littersInTank = 0;
+int sumOfQuantities = 0;
+for(int i = 0; i <= numberOfInput; i++)
+{
+    int quantitiesOfWater = int.Parse(Console.ReadLine());
+
+    sumOfQuantities += quantitiesOfWater;
+    if (sumOfQuantities >= waterTankCapasity)
+    {
+        Console.WriteLine("Insufficient capacity!");
+        sumOfQuantities = sumOfQuantities - quantitiesOfWater;
+        continue;
+    }
+        //Console.WriteLine(sumOfQuantities);
+}
+littersInTank = sumOfQuantities; 
+Console.WriteLine(littersInTank);
