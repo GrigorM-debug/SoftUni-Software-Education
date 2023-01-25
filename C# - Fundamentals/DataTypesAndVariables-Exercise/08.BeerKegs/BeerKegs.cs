fc817f53bda459b738a198491c1535b033627a0a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-25 16:41:26 +0200

    new task for beersKegs

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/08.BeerKegs/BeerKegs.cs
@@ -0,0 +1,21 @@
+﻿int numberOfInputs = int.Parse(Console.ReadLine());
+double max = double.MinValue;
+double volume = 0.0;
+string model = "";
+string biggestKeg = "";
+for (int i = 0; i < numberOfInputs; i++)
+{
+    model = Console.ReadLine();
+    double rasdius = double.Parse(Console.ReadLine());  
+    int height = int.Parse(Console.ReadLine());
+
+    volume = Math.PI * (rasdius * rasdius) * height;
+    
+    if (volume > max)
+    {
+        max = volume;
+        biggestKeg = model; 
+    }
+}
+Console.WriteLine(biggestKeg);
+
