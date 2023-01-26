c4c459287ba0c3b015348771e3a8f7684e652ebf
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-26 13:57:12 +0200

    new task

--- C# - Fundamentals/DataTypesAndVariables-Exercise/07.WaterOverflow/WaterOverflow.cs
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/07.WaterOverflow/WaterOverflow.cs
@@ -1,21 +1,22 @@
 ﻿int waterTankCapasity = 255;
 int numberOfInput = int.Parse(Console.ReadLine());
-int littersInTank = 0;
 int sumOfQuantities = 0;
-for(int i = 0; i <= numberOfInput; i++)
+
+for (int i = 0; i <= numberOfInput; i++)
 {
     int quantitiesOfWater = int.Parse(Console.ReadLine());
 
     sumOfQuantities += quantitiesOfWater;
-    if (sumOfQuantities >= waterTankCapasity)
+
+    if (sumOfQuantities > waterTankCapasity)
     {
         Console.WriteLine("Insufficient capacity!");
         sumOfQuantities = sumOfQuantities - quantitiesOfWater;
+        Console.WriteLine(sumOfQuantities);
         continue;
     }
-        //Console.WriteLine(sumOfQuantities);
+    else
+    {
+        Console.WriteLine(sumOfQuantities);
+    }
 }
-littersInTank = sumOfQuantities; 
-Console.WriteLine(littersInTank);
-
-//Довърши тази задача
