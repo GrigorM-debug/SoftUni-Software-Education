131cebcd163f1cd4593912cbf8abd36c23c547cd
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-26 14:52:01 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/10.Pokemon/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿int power = int.Parse(Console.ReadLine());
+int distance = int.Parse(Console.ReadLine());  
+int factor = int.Parse(Console.ReadLine());
+
+int pokedTargetsCnt = 0;
+double percent = power * 0.5;
+while (power >= distance)
+{
+    pokedTargetsCnt++;
+    power = power - distance;
+    if (power == percent && factor !=0)
+    {
+        power = power / factor;
+        if (power < distance)
+        {
+            break;
+        }
+    }
+}
+Console.WriteLine(power);
+Console.WriteLine(pokedTargetsCnt);
