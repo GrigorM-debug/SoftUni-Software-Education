15083d77fff70226440a246c221c12e1918fbd34
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-26 13:01:32 +0200

    new task

---
+++ C# - Fundamentals/DataTypesAndVariables-Exercise/09.SpiceMustFlow/SpiceMustFlow.cs
@@ -0,0 +1,22 @@
+﻿int startingYield = int.Parse(Console.ReadLine());
+
+int days = 0;
+int produced = 0;
+
+if (startingYield >= 100)
+{
+    while (startingYield >= 100)
+    {
+        produced += startingYield - 26;
+        startingYield -= 10;
+        days++;
+    }
+    produced -= 26;
+    Console.WriteLine(days);
+    Console.WriteLine(produced);
+}
+else
+{
+    Console.WriteLine(days);
+    Console.WriteLine(produced);
+}
