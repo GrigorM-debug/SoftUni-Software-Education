31196cfa05b883c5bb9228e6f1720cc2d4598d9c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-29 00:58:04 +0200

    new task

---
+++ C# - Fundamentals/Arrays-Lab/06.EvenAndOddSubstraction/EvenAndOddSubstraction.cs
@@ -0,0 +1,21 @@
+﻿int sumOfEvenNumbers = 0;
+int sumOfOddNumbers = 0;
+int difference = 0;
+
+int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i < numbers.Length;i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        sumOfEvenNumbers += numbers[i];
+    }
+    else
+    {
+        sumOfOddNumbers += numbers[i];          
+    }
+}difference = sumOfEvenNumbers - sumOfOddNumbers;
+Console.WriteLine(difference);
