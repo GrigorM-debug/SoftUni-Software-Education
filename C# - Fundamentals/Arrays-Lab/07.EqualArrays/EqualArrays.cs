2259405f8cfa0138fe14b5681bd2088e02a41436
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-29 01:49:26 +0200

    new task

---
+++ C# - Fundamentals/Arrays-Lab/07.EqualArrays/EqualArrays.cs
@@ -0,0 +1,24 @@
+﻿using System.Security;
+
+int[] firstArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] secondArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int sumOfArrays = 0;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i] != secondArray[i])
+    {
+        Console.Write($"Arrays are not identical. Found difference at {i} index");
+        return;
+    }
+}
+sumOfArrays = firstArray.Sum();
+Console.Write($"Arrays are identical. Sum: {sumOfArrays}");
