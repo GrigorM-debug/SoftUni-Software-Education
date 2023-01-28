b6d688b057ab62b13ce1779f08f2a8635a6d0b6e
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-28 18:11:40 +0200

    new task

---
+++ C# - Fundamentals/Arrays-Lab/02.PrintNumbersInReverseOrder/PrintNumbersInReverseOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+int numbersOfInputs = int.Parse(Console.ReadLine());
+
+int[] numbers = new int [numbersOfInputs];
+
+for (int i = 0; i< numbersOfInputs; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    numbers[i] = n;
+}
+
+int[] reversedNumbers = numbers.Reverse().ToArray();
+
+for (int i = 0; i < reversedNumbers.Length; i++)
+{
+    Console.Write(reversedNumbers[i] + " ");
+}    
+
+
+//Second for reversing the numbers 
+/* for (int i = numbers.Lenght-1; i = 0; i--)
+ {
+    Console.Write(numbers[i] + " ");
+ }
+
+*/
---
+++ C# - Fundamentals/Arrays-Lab/04.ReverseArrayOfStrings/ReverseArrayOfStrings.cs
@@ -0,0 +1,9 @@
+﻿string[] arrayOfStrings = Console.ReadLine().Split().ToArray();
+
+string[] reversed = arrayOfStrings.Reverse().ToArray(); 
+
+for ( int i = 0; i < reversed.Length; i++)
+{
+    Console.Write(reversed[i] + " ");
+}
+
