565436bda95c45d0b57f3fe5469f823423b0efc4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-29 15:53:19 +0200

    new task for new solution

---
+++ C# - Fundamentals/Arrays - Exercise/01.Train/Train.cs
@@ -0,0 +1,13 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+int[] people = new int[input];
+
+for (int i = 0; i < people.Length; i++)
+{
+    people[i] = int.Parse(Console.ReadLine()); 
+
+}
+Console.WriteLine(string.Join(" ", people));
+int peopleCount = people.Sum();
+
+Console.WriteLine(peopleCount); 
