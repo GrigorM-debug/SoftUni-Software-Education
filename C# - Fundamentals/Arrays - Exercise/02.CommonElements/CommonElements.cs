b000a67fa3526e2b168bdfe1a8ba918be5ab0719
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-01-29 16:00:10 +0200

    new task

---
+++ C# - Fundamentals/Arrays - Exercise/02.CommonElements/CommonElements.cs
@@ -0,0 +1,10 @@
+﻿string[] firstArray = Console.ReadLine().Split();
+string[] secondArray = Console.ReadLine().Split();
+
+for (int i = 0; i < secondArray.Length; i++)
+{
+    if (firstArray.Contains(secondArray[i]))
+    {
+        Console.Write(secondArray[i] + " ");
+    }
+}
