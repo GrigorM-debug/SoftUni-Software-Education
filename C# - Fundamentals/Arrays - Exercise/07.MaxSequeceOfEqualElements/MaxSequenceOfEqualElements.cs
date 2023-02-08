b9006d608f47063bc42a289771c0390d5e40f9a7
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-08 23:58:21 +0200

    new task

---
+++ C# - Fundamentals/Arrays - Exercise/07.MaxSequeceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int mostNumber = 0; 
+int longestSequece = 0;
+int counter = 1;
+
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    if (arr[i] == arr[i+1])
+    {
+        counter++;
+    }
+    else
+    {
+        counter = 1;
+    }
+
+    if (counter > mostNumber)
+    {
+        mostNumber= counter;
+        longestSequece = arr[i];
+    }
+}
+for (int i = 0; i < mostNumber; i++)
+{
+    Console.Write(longestSequece + " ");
+}
