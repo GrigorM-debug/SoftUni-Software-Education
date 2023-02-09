ac968ec78ea7527a53e2a8a5127c599c39857898
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-09 23:52:19 +0200

    new task

---
+++ C# - Fundamentals/Arrays - Exercise/08.MagicSum/MagicSum.cs
@@ -0,0 +1,23 @@
+﻿int[] arr = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int number = int.Parse(Console.ReadLine());
+
+int specialNumbers = 0;
+
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    int sumOfElements = 0;
+
+    for (int k = i + 1; k < arr.Length; k++)
+    {
+        sumOfElements= arr[i] + arr[k];
+
+        if (sumOfElements == number)
+        {
+            Console.WriteLine(string.Join(" ", arr[i], arr[k]));
+        }
+    }
+}
