941597d97190c633497db1c87998f013bae20ff9
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-10 17:43:01 +0200

    new task from new lection about Methods

---
+++ C# - Fundamentals/Methods-Lab/03.Calculations/Calculations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _03.Calculations
+{
+    internal class Calculations
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());  
+
+            switch(command)
+            {
+                case "add":
+                    Add(numberOne, numberTwo);
+                    break;
+                case "subtract":
+                    Subtract(numberOne, numberTwo);
+                    break;
+                case "multiply":
+                    Multiply(numberOne, numberTwo);   
+                  break;
+                case "divide":
+                    Divide(numberOne, numberTwo);   
+                    break;
+            }
+        }
+        static void Add(int numberOne, int numberTwo)
+        {
+            int result = numberOne+ numberTwo;
+            Console.WriteLine(result);
+        }
+
+        static void Subtract(int numberOne, int numberTwo)
+        {
+            int result = numberOne - numberTwo;
+            Console.WriteLine(result);
+        }
+
+        static void Multiply(int numberOne, int numberTwo)
+        { 
+            int result = numberOne * numberTwo;
+            Console.WriteLine(result);
+        }
+
+        static void Divide(int numberOne, int numberTwo)
+        {
+            int result = numberOne / numberTwo;
+            Console.WriteLine(result);
+        }
+    }
+}
