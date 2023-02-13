0613c15e664aa43b1a3d5d6da8035e51aebee57c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-13 18:28:10 +0200

    new task

---
+++ C# - Fundamentals/Methods - Exercise/03.CharacterRange/CharacterRange.cs
@@ -0,0 +1,22 @@
+﻿internal class CharacterRange
+{
+    static void Main()
+    {
+        char firstCharacter = char.Parse(Console.ReadLine());
+        char secondCharacter = char.Parse(Console.ReadLine());
+
+        PrintCharacter(firstCharacter, secondCharacter);
+       
+    }
+    static void PrintCharacter (char firstCharacter, char secondCharacter)
+    {
+        int startChracter = Math.Min(firstCharacter, secondCharacter);
+        int endCharacter = Math.Max(firstCharacter, secondCharacter);
+
+        for (int i = ++startChracter; i < endCharacter; i++)
+        {
+            char currentCharacter = (char)i;
+            Console.Write(currentCharacter + " ");
+        }
+    }
+}
