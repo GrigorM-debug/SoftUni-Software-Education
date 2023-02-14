5cd42c9b3629b2ee0baae62a81c35c43bfdd1e7a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-14 07:46:02 +0200

    new task

--- C# - Fundamentals/Methods - Exercise/10.TopNumber/TopNumber.cs
+++ C# - Fundamentals/Methods - Exercise/10.TopNumber/TopNumber.cs
@@ -1,11 +1,39 @@
 ﻿using System.Globalization;
+using System.Security;
 
-internal class TopNumber
+internal class TopNumberFromNumbers
 {
     static void Main()
     {
-        string n = Console.ReadLine();
-        bool isTopNuimber = TopNumber(n);
-        Console.WriteLine(isTopNuimber.ToString().ToLower());
+        int number = int.Parse(Console.ReadLine());
+        TopNumber(number);
+    }
+
+    static void TopNumber(int number)
+    {
+        int sumOfDigits = 0;
+        bool isDifferentFromZero = false;
+
+        for (int i = 1; i <= number; i++)
+        {
+            int num = i;
+            int curentNum = 0;
+            bool isDiferentFromZero = false;
+
+            while (num > 0)
+            {
+                curentNum += num % 10;
+                if (num % 2 != 0)
+                {
+                    isDiferentFromZero = true;
+                }
+
+                num /= 10;
+            }
+            if (curentNum % 8 == 0 && isDiferentFromZero == true)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
