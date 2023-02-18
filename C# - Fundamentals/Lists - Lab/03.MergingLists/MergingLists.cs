35f2e7a2f58017e973da653f02ae179eedfaef18
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-18 13:56:34 +0200

    new task

---
+++ C# - Fundamentals/Lists - Lab/03.MergingLists/MergingLists.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Runtime.ExceptionServices;
+
+List<int> listOne = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> listTwo = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> result = new List<int>();
+
+int minList = Math.Min(listOne.Count, listTwo.Count);
+
+for(int i = 0; i < minList; i++)
+{
+    result.Add(listOne[i]);
+    result.Add(listTwo[i]);
+}
+
+if (listOne.Count > listTwo.Count)
+{
+    for (int i = minList; i < listOne.Count; i++)
+    {
+        result.Add(listOne[i]); 
+    }
+}
+else if (listOne.Count < listTwo.Count)
+{
+    for (int i = minList; i < listTwo.Count; i++)
+    {
+        result.Add(listTwo[i]);
+    }
+}
+Console.WriteLine(string.Join(" ", result));
