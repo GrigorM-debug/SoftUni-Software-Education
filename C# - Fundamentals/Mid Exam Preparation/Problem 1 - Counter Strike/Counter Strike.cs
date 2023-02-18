d7a09d81b1fa7bc23b3ef81c515f2df596fd2c4b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-18 15:53:37 +0200

    preparing for the exam

---
+++ C# - Fundamentals/Mid Exam Preparation/Problem 1 - Counter Strike/Counter Strike.cs
@@ -0,0 +1,35 @@
+﻿int initialEnergy = int.Parse(Console.ReadLine());
+
+string input  = Console.ReadLine();
+
+bool flag = true;
+int battlesWon = 0;
+
+while (input != "End of battle")
+{
+    int distance = int.Parse(input);
+
+    if (initialEnergy - distance < 0)
+    {
+        flag= false;
+        break;
+    }
+
+    initialEnergy-= distance;
+    battlesWon++;
+
+    if (battlesWon % 3 == 0)
+    {
+        initialEnergy += battlesWon;
+    }
+
+    input = Console.ReadLine(); 
+}
+if (flag)
+{
+    Console.WriteLine($"Won battles: {battlesWon}. Energy left: {initialEnergy}");
+}
+else
+{
+    Console.WriteLine($"Not enough energy! Game ends with {battlesWon} won battles and {initialEnergy} energy");
+}
