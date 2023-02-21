656c01eea25ae176b3a1ac4dd7b12203c05d1a35
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-21 19:01:51 +0200

    new task

---
+++ C# - Fundamentals/MidExamPreparation-15-02-2023/Problem 1 - Black Flag/BlackFlag.cs
@@ -0,0 +1,28 @@
+﻿double days = double.Parse(Console.ReadLine());
+double dailyPlunder = double.Parse(Console.ReadLine());   
+double expectedPlunder = double.Parse(Console.ReadLine());
+
+double gainedPlunder = 0.0;
+
+for (double i = 1; i <= days; i++)
+{
+    gainedPlunder += dailyPlunder;
+
+    if (i % 3 == 0)
+    {
+        gainedPlunder += dailyPlunder * 0.5;
+    }
+    if (i % 5 == 0) 
+    {
+        gainedPlunder -= gainedPlunder * 0.3;
+    }
+}
+if (gainedPlunder >= expectedPlunder)
+{
+    Console.WriteLine($"Ahoy! {gainedPlunder:F2} plunder gained.");
+}
+else if (gainedPlunder < expectedPlunder)
+{
+    double gainedPlunderInPercentage = (gainedPlunder / expectedPlunder) * 100;
+    Console.WriteLine($"Collected only {gainedPlunderInPercentage:F2}% of the plunder.");
+}
