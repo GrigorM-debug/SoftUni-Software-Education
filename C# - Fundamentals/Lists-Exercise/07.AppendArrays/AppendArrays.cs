d5950fd77f3218bc105c1c171278fc07d4978ef6
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-21 17:48:01 +0200

    new task

---
+++ C# - Fundamentals/Lists-Exercise/07.AppendArrays/AppendArrays.cs
@@ -0,0 +1,11 @@
+﻿List<string> numbers = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+numbers.Reverse();
+List<string> numbersSplit = new List<string>();
+
+foreach (var number in numbers)
+{
+    numbersSplit.AddRange(number.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList());
+}
+
+Console.WriteLine(string.Join(" ", numbersSplit));
