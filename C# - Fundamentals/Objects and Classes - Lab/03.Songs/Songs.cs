fa351c0f9e98086b45fee0fc5f329e4777a15dd7
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-23 22:23:01 +0200

    new tasks

---
+++ C# - Fundamentals/Objects and Classes - Lab/03.Songs/Songs.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+int n = int.Parse(Console.ReadLine());
+
+List<Song> songs = new List<Song>();    
+
+for (int i = 1; i <= n; i++)
+{
+    string[] songInfo = Console.ReadLine().Split("_");
+
+    string type = songInfo[0];  
+    string songName = songInfo[1];  
+    string duration = songInfo[2];
+
+    Song song = new Song();
+    {
+        song.TypeList = type;
+        song.Name = songName;
+        song.Time = duration;
+    };
+
+    songs.Add(song);
+}
+
+string typeList = Console.ReadLine();
+
+if (typeList == "all")
+{
+    foreach(Song song in songs)
+    {
+        Console.WriteLine(song.Name);
+    }
+}
+else
+{
+    foreach (Song song in songs)
+    {
+        if (song.TypeList == typeList)
+        {
+            Console.WriteLine(song.Name);
+        }
+    }
+}
+
+
+class Song
+{
+    public string TypeList { get; set; }
+
+    public string Name { get; set; }    
+
+    public string Time { get; set; }   
+}
---
+++ C# - Fundamentals/Objects and Classes - Lab/04.Students/Students.cs
@@ -0,0 +1,45 @@
+﻿string input = Console.ReadLine();
+
+List<Student> students = new List<Student>();
+
+while (input != "end")
+{
+    string[] studentInfo = input.Split();
+
+    string firstName = studentInfo[0];  
+    string lastName = studentInfo[1];
+    int age = int.Parse(studentInfo[2]);    
+    string homeTown = studentInfo[3];   
+
+    Student student = new Student();
+    {
+        student.FirstName = firstName;
+        student.LastName = lastName;
+        student.Age = age;
+        student.Hometown = homeTown;
+    };
+    students.Add(student);
+
+    input = Console.ReadLine(); 
+}
+string cityName = Console.ReadLine();
+
+foreach (Student student in students)
+{
+    if (student.Hometown == cityName)
+    {
+        Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+    }
+}
+
+
+class Student
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }    
+
+    public string Hometown { get; set; }
+}
