5e010ddd3fc2d09f05cbd20072906f53b5decadf
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-26 19:39:25 +0200

    new task for friday lection

---
+++ C# - Fundamentals/Objects and Classes - Exercise/01.AdvertisementMessage/AdvertisementMessage.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System;
+int numberOfGeneratateMassages = int.Parse(Console.ReadLine());
+
+string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+
+string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+
+string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+for (int i = 0; i < numberOfGeneratateMassages; i++)
+{
+    Random random = new Random();
+    string randomPhrases = phrases[random.Next(phrases.Length)];
+    string randomEvents = events[random.Next(events.Length)];
+    string randomAuthors = authors[random.Next(authors.Length)];
+    string randomCities = cities[random.Next(cities.Length)];
+
+    Console.WriteLine($"{randomPhrases} {randomEvents} {randomAuthors} - {randomCities}");
+}
