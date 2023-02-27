c100c5c4568138921b5c72da960dd57072031a52
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-27 18:49:55 +0200

    new task

---
+++ C# - Fundamentals/Objects and Classes - Exercise/04.Students/Students.cs
@@ -0,0 +1,42 @@
+﻿int countOfStudents = int.Parse(Console.ReadLine());
+
+List<Students> students = new List<Students>();
+
+for (int i = 0; i < countOfStudents; i++)
+{
+    string[] studentsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    string firstName = studentsInfo[0];
+    string lastName = studentsInfo[1];
+    double grade = double.Parse(studentsInfo[2]);
+
+    Students student = new Students(firstName, lastName, grade);
+
+    students.Add(student);
+}
+
+foreach (var student in students.OrderByDescending(x=>x.Grade))
+{
+    Console.WriteLine(student);
+}
+
+class Students
+{
+    public Students(string firstName, string lastName, double grade) 
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+    public string FirstName { get; set; }    
+
+    public string LastName { get; set; }
+
+    public double Grade { get; set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}: {Grade:f2}";
+    }
+}
---
+++ C# - Fundamentals/Objects and Classes - Exercise/07.OrderedByAge/OrderedByAge.cs
@@ -0,0 +1,48 @@
+﻿string input = Console.ReadLine();
+
+
+List<Persons> persons = new List<Persons>();    
+
+while(input != "End")
+{
+    string[] personInfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    string personName = personInfo[0];  
+    int personID = int.Parse(personInfo[1]);
+    int personAge  = int.Parse(personInfo[2]);
+
+    Persons person = new Persons(personName, personID, personAge);
+
+    persons.Add(person);
+
+    input = Console.ReadLine();
+}
+
+
+foreach(var person in persons.OrderBy(x => x.Age))
+{
+   Console.WriteLine(person);
+}
+
+class Persons
+{
+    public Persons(string personName, int personID, int personAge)
+    {
+        Name = personName;  
+        ID = personID;
+        Age = personAge;
+    }
+    
+
+    public string Name { get; set; }    
+
+    public int ID { get; set; }
+
+    public int Age { get; set; }
+
+
+    public override string ToString()
+    {
+        return $"{Name} with ID: {ID} is {Age} years old.";
+    }
+}
