96a2b5f78e7c1dbdbba7d9a7a7bd3af4340217d4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-04 18:13:39 +0200

    new task

---
+++ C# - Fundamentals/Associative Arrays - Lab/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,18 @@
+﻿double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+SortedDictionary<double, int> numbersSorted = new SortedDictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (!numbersSorted.ContainsKey(number))
+    {
+        numbersSorted.Add(number, 0);
+    }
+
+    numbersSorted[number] += 1;
+}
+
+foreach (var number in numbersSorted)
+{
+    Console.WriteLine($"{number.Key} -> {number.Value}");
+}
