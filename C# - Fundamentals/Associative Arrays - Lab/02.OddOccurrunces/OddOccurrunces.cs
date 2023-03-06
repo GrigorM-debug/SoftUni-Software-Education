77f8d8cf40ff445581fe23b0b6dabf776553886d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-06 17:45:47 +0200

    new task

---
+++ C# - Fundamentals/Associative Arrays - Lab/02.OddOccurrunces/OddOccurrunces.cs
@@ -0,0 +1,19 @@
+﻿string[] words = Console.ReadLine().Split();
+
+Dictionary<string, int> allWords = new Dictionary<string, int>();
+
+foreach(string word in words)
+{
+    string wordsLower = word.ToLower();
+
+    if (!allWords.ContainsKey(wordsLower))
+    {
+        allWords.Add(wordsLower, 0);
+    }
+
+    allWords[wordsLower]++; 
+}
+
+var result = allWords.Where(x => x.Value % 2 != 0).Select(x => x.Key).ToList();
+
+Console.WriteLine(string.Join(" ", result));
---
+++ C# - Fundamentals/Associative Arrays - Lab/03.WordSynonyms/WordsSynonyms.cs
@@ -0,0 +1,20 @@
+﻿int numberOfInputs = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> words= new Dictionary<string, List<string>>();
+
+for(int i = 0; i < numberOfInputs; i++)
+{
+    string word = Console.ReadLine();   
+    string synonym = Console.ReadLine();    
+
+    if (!words.ContainsKey(word))
+    {
+        words.Add(word, new List<string>());
+    }
+    words[word].Add(synonym);
+}
+
+foreach (var word in words)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
