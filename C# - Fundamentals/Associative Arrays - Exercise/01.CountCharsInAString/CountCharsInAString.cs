d665a3c51a74868ed194e3b7e7bb4622afb297a6
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-06 19:41:49 +0200

    new task for new lection

---
+++ C# - Fundamentals/Associative Arrays - Exercise/01.CountCharsInAString/CountCharsInAString.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Tracing;
+
+var letters = new Dictionary<char, int>();
+
+string word = Console.ReadLine();
+
+foreach (var currChar in word)
+{
+    if (currChar == ' ')
+    {
+        continue;
+    }
+
+    if(!letters.ContainsKey(currChar))
+    {
+        letters.Add(currChar, 0);
+    }
+
+    letters[currChar]++;
+}
+
+foreach(var item in letters)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
---
+++ C# - Fundamentals/Associative Arrays - Exercise/02.AMinerTask/AMinerTask.cs
@@ -0,0 +1,26 @@
+﻿var mine = new Dictionary<string, int>();
+
+string input = Console.ReadLine();
+
+while (input != "stop")
+{
+    string resouces = input;
+    int quantity = int.Parse(Console.ReadLine());
+
+    if(!mine.ContainsKey(resouces))
+    {
+        mine.Add(resouces, 0);
+    }
+
+    mine[resouces] += quantity;
+
+    input = Console.ReadLine();
+}
+
+foreach(var currentResources in mine)
+{
+    Console.WriteLine($"{currentResources.Key} -> {currentResources.Value}");
+}
+
+
+
