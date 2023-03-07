deb033bf9ef8d593386c53274843717c6aec101f
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-07 17:31:11 +0200

    new task

---
+++ C# - Fundamentals/Associative Arrays - Exercise/03.Orders/Orders.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<string, List<double>> orders = new Dictionary<string, List<double>>();
+
+string input = Console.ReadLine();
+
+while (input != "buy")
+{
+    string[] cmdInfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string productName = cmdInfo[0];
+    double productPrice = double.Parse(cmdInfo[1]);
+    double productQuantity = double.Parse(cmdInfo[2]);
+
+    if (!orders.ContainsKey(productName))
+    {
+        orders.Add(productName, new List<double>() { productPrice, productQuantity});
+    }
+    else
+    {
+        orders[productName][0] = productPrice;
+        orders[productName][1] += productQuantity;
+    }
+
+    input= Console.ReadLine();
+}
+
+foreach (var order in orders)
+{
+    double totalPrice = order.Value[0] * order.Value[1];
+
+    Console.WriteLine($"{order.Key} -> {totalPrice:f2}");
+}
---
+++ C# - Fundamentals/Associative Arrays - Exercise/04.SoftUniParking/SoftUniParking.cs
@@ -0,0 +1,43 @@
+﻿Dictionary<string, string> parking = new Dictionary<string, string>();  
+
+int numberOfCommands = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfCommands; i++)
+{
+    string[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = cmdArgs[0];
+    string username = cmdArgs[1];
+
+    if (command == "register")
+    {
+        string plateNumber = cmdArgs[2];
+
+        if (parking.ContainsKey(username))
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+        }
+        else
+        {
+            parking[username] = plateNumber;
+            Console.WriteLine($"{username} registered {plateNumber} successfully");
+        }
+    }
+    else if (command == "unregister")
+    {
+        if(!parking.ContainsKey(username))
+        {
+            Console.WriteLine($"ERROR: user {username} not found");
+        }
+        else
+        {
+            parking.Remove(username);
+            Console.WriteLine($"{username} unregistered successfully");
+        }
+    }
+}
+
+foreach(var car in parking)
+{
+    Console.WriteLine($"{car.Key} => {car.Value}");
+}
