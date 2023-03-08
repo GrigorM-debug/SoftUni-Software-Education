c28d0fa2566dd2b23185cafd0d85175b3b7b45d9
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-08 19:07:47 +0200

    new task

---
+++ C# - Fundamentals/Associative Arrays - Exercise/06.StudentAcademy/StudentAcademy.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+Dictionary<string, List<double>> academy = new Dictionary<string, List<double>>();  
+
+int numberOfInputs = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfInputs; i++)
+{
+    string studentName = Console.ReadLine();    
+    double grade = double.Parse(Console.ReadLine());    
+
+    if (!academy.ContainsKey(studentName))
+    {
+        academy.Add(studentName, new List<double>());
+
+    }
+
+    academy[studentName].Add(grade);
+}
+
+foreach(var student in academy)
+{
+    if (student.Value.Average() >= 4.5)
+    {
+        Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+    }
+}
