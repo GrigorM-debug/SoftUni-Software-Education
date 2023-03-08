2ae5a887488fcb17bc495f5b5c2cb568e2443c14
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-08 18:46:08 +0200

    new task

---
+++ C# - Fundamentals/Associative Arrays - Exercise/05.Courses/Courses.cs
@@ -0,0 +1,35 @@
+﻿Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+    string[] courseArgs = Console.ReadLine().Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+    string courseName = courseArgs[0];
+    string studentName = courseArgs[1]; 
+
+    if (!courses.ContainsKey(courseName))
+    {
+        courses.Add(courseName, new List<string>());
+    }
+    else
+    {
+        courses[courseName].Add(studentName);
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach(var course in courses)
+{
+    Console.WriteLine($"{course.Key}: {courses.Count}");
+    
+    foreach(var user in course.Value) 
+    {
+        Console.WriteLine($"--{user}");
+    }
+}
+
+//TODO
---
+++ C# - Fundamentals/Associative Arrays - Exercise/07.CompanyUsers/CompanyUsers.cs
@@ -0,0 +1,33 @@
+﻿Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+while(input!="End")
+{
+    string[] cmdArgs = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+    string company = cmdArgs[0];
+    string employeedId = cmdArgs[1];
+
+    if (!companyUsers.ContainsKey(company))
+    {
+        companyUsers[company] = new List<string>();
+    }
+
+    if (!companyUsers[company].Contains(employeedId))
+    {
+        companyUsers[company].Add(employeedId);
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach(var company in companyUsers)
+{
+    Console.WriteLine($"{company.Key}");
+
+    foreach(var user in company.Value)
+    {
+        Console.WriteLine($"-- {user}");
+    }
+}
