b101df5dd1e0027c630678842fd2552191147dc2
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-11 14:38:23 +0200

    fixing on of the tasks

--- C# - Fundamentals/Associative Arrays - Exercise/05.Courses/Courses.cs
+++ C# - Fundamentals/Associative Arrays - Exercise/05.Courses/Courses.cs
@@ -5,31 +5,27 @@ string input = Console.ReadLine();
 
 while (input != "end")
 {
-    string[] courseArgs = Console.ReadLine().Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+    string[] courseArgs = input.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
 
     string courseName = courseArgs[0];
-    string studentName = courseArgs[1]; 
+    string studentName = courseArgs[1];
 
     if (!courses.ContainsKey(courseName))
     {
         courses.Add(courseName, new List<string>());
     }
-    else
-    {
-        courses[courseName].Add(studentName);
-    }
+
+    courses[courseName].Add(studentName);
 
     input = Console.ReadLine();
 }
 
 foreach(var course in courses)
 {
-    Console.WriteLine($"{course.Key}: {courses.Count}");
+    Console.WriteLine($"{course.Key}: {course.Value.Count}");
     
     foreach(var user in course.Value) 
     {
-        Console.WriteLine($"--{user}");
+        Console.WriteLine($"-- {user}");
     }
 }
-
-//TODO
