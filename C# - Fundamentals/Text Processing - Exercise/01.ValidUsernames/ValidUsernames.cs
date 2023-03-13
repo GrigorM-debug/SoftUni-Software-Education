878856e322d5b51cc2fcdd924fd26d1bd9030347
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-13 19:36:02 +0200

    new task

---
+++ C# - Fundamentals/Text Processing - Exercise/01.ValidUsernames/ValidUsernames.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string[] names = input.Split(", ");
+
+        foreach (string name in names)
+        {
+            if (IsUserNameValid(name))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+
+    static bool IsUserNameValid(string name)
+    {
+        if (name.Length < 3 || name.Length > 16)
+        {
+            return false;
+        }
+
+        foreach (var character in name)
+        {
+            if (character != '_' && character != '-' && !char.IsLetterOrDigit(character))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
---
+++ C# - Fundamentals/Text Processing - Exercise/06.ReplaceRepeatingChars/ReplaceRepeatingChars.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+string input  = Console.ReadLine();
+
+StringBuilder newstring = new StringBuilder();      
+for (int i = 0; i < input.Length; i++)
+{
+    while (i < input.Length-1 && input[i] == input[i + 1])
+    {
+        i++;
+    }
+
+    newstring.Append(input[i]);
+}
+Console.WriteLine(newstring.ToString());
