0513a247752476f31aa2dc85af58872e7b1aa93c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-15 17:08:13 +0200

    new task

---
+++ C# - Fundamentals/Arrays - More Exercise/02.PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,23 @@
+﻿int numberOfInputs = int.Parse(Console.ReadLine());
+
+int[] arr = null;
+int[] secondArr = null;
+
+for (int i = 1;i <= numberOfInputs; i++)
+{
+    arr = new int[i];
+
+    arr[0] = 1;
+    arr[arr.Length-1] = 1;
+
+    for (int j = 1; j < arr.Length-1;j++)
+    {
+        arr[j] = secondArr[j-1] + secondArr[j];
+    }
+
+    secondArr = arr;
+
+    Console.WriteLine(string.Join(" ", arr));
+}
+
+
