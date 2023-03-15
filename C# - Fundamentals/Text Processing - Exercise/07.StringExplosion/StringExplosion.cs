48c017acaa7ffddf2d3b2bd0c001615ba3921741
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-15 16:16:38 +0200

    new task

---
+++ C# - Fundamentals/Text Processing - Exercise/07.StringExplosion/StringExplosion.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+
+var outputText = new StringBuilder();
+
+int bompPower = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    char currentCharacter = input[i];   
+
+    if (currentCharacter == '>')
+    {
+        int currentBompPower = int.Parse(input[i+1].ToString());
+
+        outputText.Append(currentCharacter);
+
+        bompPower += currentBompPower;
+    }
+    else
+    {
+        if (bompPower > 0)
+        {
+            bompPower--;
+        }
+        else
+        {
+            outputText.Append(currentCharacter);
+        }
+    }
+}
+Console.WriteLine(outputText.ToString());
