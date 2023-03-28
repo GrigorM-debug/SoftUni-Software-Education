caab818ea1ebe2566dbb715254282a2709e5d8dd
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-28 16:52:58 +0300

    Preparing for exam

---
+++ C# - Fundamentals/Exam Preparation - 28.03.2023/02.FancyBarcodes/FancyBarcodes.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"@\#+(?<product>[A-Z][A-Za-z0-9]{4,}[A-Z])@\#+";
+
+int numberOfinputs = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfinputs; i++)
+{
+    string barcode = Console.ReadLine();
+
+    Match match = Regex.Match(barcode, pattern);
+
+    if (match.Success)
+    {
+        string product = match.Groups["product"].Value;
+        string productGroup = string.Empty;
+
+        foreach (char curr in product)
+        {
+            if (Char.IsDigit(curr))
+            {
+                productGroup += curr;
+            }
+
+        }
+        if (productGroup == string.Empty)
+        {
+            productGroup = "00";
+        }
+        Console.WriteLine($"Product group: {productGroup}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid barcode");
+    }
+}
