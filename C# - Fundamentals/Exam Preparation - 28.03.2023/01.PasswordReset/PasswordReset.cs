970f0267ff7a8f48b3916f52e5c33d076ec4516b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-03-28 16:16:16 +0300

    Preparing for the exam

---
+++ C# - Fundamentals/Exam Preparation - 28.03.2023/01.PasswordReset/PasswordReset.cs
@@ -0,0 +1,54 @@
+﻿using System.Security;
+using System.Text;
+
+string password = Console.ReadLine();
+
+string input = Console.ReadLine();
+
+while (input != "Done")
+{
+    string[] commandArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = commandArgs[0];
+
+    if (command == "TakeOdd")
+    {
+        string odd = string.Empty;
+        for (int i =0; i < password.Length; i++)
+        {
+            if (i % 2 != 0)
+            {
+                odd += password[i];
+            }
+        }
+        Console.WriteLine(odd);
+        password = odd;
+    }
+    else if (command == "Cut")
+    {
+        int index = int.Parse(commandArgs[1]);
+        int lenght = int.Parse(commandArgs[2]);
+
+        password = password.Remove(index, lenght);
+
+        Console.WriteLine(password);
+    }
+    else if (command == "Substitute")
+    {
+        string substring = commandArgs[1];
+        string stringForReplacemnt = commandArgs[2];
+        if (password.Contains(substring))
+        {
+            password = password.Replace(substring, stringForReplacemnt);
+
+            Console.WriteLine(password);
+        }
+        else
+        {
+            Console.WriteLine("Nothing to replace!");
+        }
+    }
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"Your password is: {password}");
