45cd797c0f1882b29c7c0c135659ad9b162b1ac4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-16 10:01:55 +0300

    8 task

---
+++ C# - Advanced/Stacks and Queues - Exercise/08.BalancedParenthesis/BalancedParenthesis.cs
@@ -0,0 +1,45 @@
+﻿string parentheses = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>();
+
+foreach (char parenthes in parentheses)
+{
+    switch (parenthes)
+    {
+        case '(':
+        case '[':
+        case '{':
+            stack.Push(parenthes);
+            break;
+        case ')':
+            if (stack.Count == 0 || stack.Pop() != '(')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+        case ']':
+            if (stack.Count == 0 || stack.Pop() != '[')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+        case '}':
+            if (stack.Count == 0 || stack.Pop() != '{')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            break;
+    }
+}
+
+if (stack.Count > 0)
+{
+    Console.WriteLine("NO");
+}
+else
+{
+    Console.WriteLine("YES");
+}
