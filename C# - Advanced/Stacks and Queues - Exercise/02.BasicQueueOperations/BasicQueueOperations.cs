088196a832967f69a65a6da3a41d4a3c94747677
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-16 09:30:19 +0300

    seccond tassk

---
+++ C# - Advanced/Stacks and Queues - Exercise/02.BasicQueueOperations/BasicQueueOperations.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+int[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int numbersToPop = tokens[1];
+int numbersToSearchFor = tokens[2];
+
+Queue<int> queue = new Queue<int>(numbers);    
+
+for (int i = 0; i < numbersToPop; i++)
+{
+    queue.Dequeue();
+}
+
+
+if (queue.Contains(numbersToSearchFor))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    if (queue.Any())
+    {
+        Console.WriteLine(queue.Min());
+    }
+    else
+    {
+        Console.WriteLine(0);
+    }
+}
