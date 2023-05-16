13be71078f774183908bbdf02fac550473f38bb7
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-16 10:32:47 +0300

    solution

---
+++ C# - Advanced/Stacks and Queues - Exercise/06.SongsQueue/SongsQueue.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+
+Queue<string> songs = new(
+    Console.ReadLine()
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+while (songs.Any())
+{
+    string[] tokens = Console.ReadLine().Split();
+    string command = tokens[0];
+
+    string song = string.Join(" ", tokens.Skip(1));
+
+    if (command == "Add")
+    {
+        if (!songs.Contains(song))
+            {
+            songs.Enqueue(song);
+        }
+        else
+        {
+            Console.WriteLine($"{song} is already contained!");
+        }
+    }
+    else if (command == "Show")
+    {
+        Console.WriteLine(string.Join(", ", songs));
+    }
+    else if (command == "Play")
+    {
+        songs.Dequeue();
+    }
+}
+if (songs.Count == 0)
+{
+    Console.WriteLine("No more songs!");
+}
+
+//This needs somr fixes
