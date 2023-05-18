40e21f899ff37cb2115744dc873385182ec67654
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-18 19:02:03 +0300

    new task

---
+++ C# - Advanced/Stacks and Queues - Exercise/10.Crossroads/Crossroads.cs
@@ -0,0 +1,46 @@
+﻿int greenLightDuration = int.Parse(Console.ReadLine());
+int freeWindowsDuration = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+
+int carPassed = 0;
+
+string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    if(input != "green")
+    {
+        cars.Enqueue(input);
+        continue;
+    }
+
+    int currGreenLghtDuration = greenLightDuration;
+
+    while (currGreenLghtDuration > 0 && cars.Any())
+    {
+        string currCar = cars.Dequeue();
+
+        if (currGreenLghtDuration - currCar.Length >= 0) 
+        { 
+            currGreenLghtDuration -= currCar.Length;
+            carPassed++;
+            continue;
+        }
+
+        if (currGreenLghtDuration + freeWindowsDuration - currCar.Length >= 0)
+        {
+            carPassed++;
+            continue;
+        }
+
+        int hittChar = currGreenLghtDuration + freeWindowsDuration;
+
+        Console.WriteLine("A crash happened!");
+        Console.WriteLine($"{currCar} was hit at {currCar[hittChar]}.");
+
+        return;
+    }
+}
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{carPassed} total cars passed the crossroads.");
