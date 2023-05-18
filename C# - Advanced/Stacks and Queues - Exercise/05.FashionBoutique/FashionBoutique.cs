800b85dc81c925fc825c9adaa04147f4b27a1178
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-18 13:15:36 +0300

    new task for key revolver

---
+++ C# - Advanced/Stacks and Queues - Exercise/05.FashionBoutique/FashionBoutique.cs
@@ -0,0 +1,21 @@
+﻿Stack<int> clothes = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+int rackCapasity = int.Parse(Console.ReadLine());
+
+int racksCount = 1;
+
+int currRackCapasity = rackCapasity;
+
+while (clothes.Any())
+{
+    currRackCapasity -= clothes.Peek();
+
+    if (currRackCapasity < 0)
+    {
+        currRackCapasity = rackCapasity;
+        racksCount++;
+        continue;
+    }
+    clothes.Pop();
+}
+Console.WriteLine(racksCount);
---
+++ C# - Advanced/Stacks and Queues - Exercise/11.KeyRevolver/KeyRevolver.cs
@@ -0,0 +1,44 @@
+﻿int priceForBullets = int.Parse(Console.ReadLine());
+int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+
+Stack<int> bullets = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());    
+
+Queue<int> locks = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+int intelligence = int.Parse(Console.ReadLine());
+
+int currSizeOfBarrel = sizeOfGunBarrel;
+
+int currBulletsCount = bullets.Count;
+
+for (int i = 0; i < currBulletsCount; i++)
+{
+    if (bullets.Pop() <= locks.Peek())
+    {
+        Console.WriteLine("Bang!");
+        locks.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+
+    currSizeOfBarrel--;
+
+    if (currSizeOfBarrel == 0 && bullets.Any())
+    {
+        Console.WriteLine("Reloading!");
+        currSizeOfBarrel= sizeOfGunBarrel;
+    }
+
+    if (!bullets.Any() && locks.Any())
+    {
+        Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+        return;
+    }
+    else if (!locks.Any())
+    {
+        Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence - (currBulletsCount - bullets.Count) * priceForBullets}");
+        return;
+    }
+}
