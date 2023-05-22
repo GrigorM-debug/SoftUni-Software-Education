fe7eb0d288de7fe6327a473a9e6ba31ac7ecc810
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-22 09:25:56 +0300

    interesting task

--- C# - Advanced/MultidimensionalArrays - Exercises/08.Bombs/Bombs.cs
+++ C# - Advanced/MultidimensionalArrays - Exercises/08.Bombs/Bombs.cs
@@ -12,21 +12,20 @@ for (int row = 0; row < size; row++)
     }
 }
 
-string[] bombsIndecxes = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+string[] bombsIndecxes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
 foreach(var  indexPair in bombsIndecxes)
 {
-    int[] indecxes = indexPair.Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-    
-    int row = indecxes[0];
-    int col = indecxes[1];
+    int[] indecxes = indexPair.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray(); ;
 
-    //TODO use method for exploding
+    int bombRow = indecxes[0];
+    int bombCol = indecxes[1];
+
+    BombExploding(matrix, bombRow, bombCol, size);
 }
 
 int aliveCellsCount = 0;
 int aliveCellsSum = 0;
-
 for (int row = 0; row < size; row++)
 {
     for (int col = 0; col < size; col++)
@@ -45,6 +44,63 @@ PrintTheMatrix(matrix, size);
 
 
 //TODO method for bomb exploding
+static void BombExploding(int[,] matrix, int bombRow, int bombCol, int size)
+{
+    int value = matrix[bombRow, bombCol];
+
+    if (value > 0)
+    {
+        matrix[bombRow, bombCol] = 0;
+
+        //up
+        if (bombRow > 0 && matrix[bombRow - 1, bombCol] > 0)
+        {
+            matrix[bombRow - 1, bombCol] -= value;
+        }
+
+        //down
+        if (bombRow < size - 1 && matrix[bombRow+1, bombCol] > 0)
+        {
+            matrix[bombRow + 1, bombCol] -= value;
+        }
+
+        //left
+        if (bombCol > 0 && matrix[bombRow, bombCol - 1] > 0)
+        {
+            matrix[bombRow, bombCol - 1] -= value;
+        }
+
+        //right
+        if (bombCol < size - 1 && matrix[bombRow, bombCol + 1] > 0)
+        {
+            matrix[bombRow, bombCol + 1] -= value;
+        }
+
+        //up and right
+        if (bombRow > 0 && bombCol < size -1 &&  matrix[bombRow - 1, bombCol + 1] > 0)
+        {
+            matrix[bombRow - 1, bombCol + 1] -= value;
+        }
+
+        //up and left
+        if (bombRow > 0 && bombCol> 0 && matrix[bombRow -1, bombCol -1] > 0)
+        {
+            matrix[bombRow - 1, bombCol -1] -= value;
+        }
+
+        //down and right
+        if (bombRow < size - 1 && bombCol < size - 1 && matrix[bombRow + 1, bombCol + 1] > 0)
+        {
+            matrix[bombRow + 1, bombCol + 1] -= value;
+        }
+
+        //down and left
+        if (bombRow < size - 1 && bombCol > 0 &&  matrix[bombRow + 1, bombCol -1] > 0)
+        {
+            matrix[bombRow + 1, bombCol - 1] -= value;
+        }
+    }
+}
 
 static void PrintTheMatrix(int[,] matrix, int size)
 {
@@ -52,7 +108,7 @@ static void PrintTheMatrix(int[,] matrix, int size)
     {
         for (int col = 0; col < size; col++)
         {
-            Console.WriteLine($"{matrix[row, col]} ");
+            Console.Write($"{matrix[row, col]} ");
         }
         Console.WriteLine();
     }
