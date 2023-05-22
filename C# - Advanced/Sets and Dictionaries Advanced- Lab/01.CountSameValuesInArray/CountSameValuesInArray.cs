822ad44b54a70f4b434f8179a085fa1b55721f78
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-22 20:33:19 +0300

    task for dictionaries advaced and later sets

---
+++ C# - Advanced/Sets and Dictionaries Advanced- Lab/01.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+
+List<decimal> input = Console.ReadLine().Split(" ").Select(decimal.Parse).ToList();
+
+Dictionary<decimal, int> numberOfOccurrences = new Dictionary<decimal, int>();
+
+foreach(  var item in input )
+{
+    if (!numberOfOccurrences.ContainsKey(item))
+    {
+        numberOfOccurrences.Add(item, 0);
+    }
+
+    numberOfOccurrences[item]++;
+}
+
+foreach(var number in numberOfOccurrences)
+{
+    Console.WriteLine($"{number.Key} - {number.Value} times");
+}  
---
+++ C# - Advanced/Sets and Dictionaries Advanced- Lab/02.AverageStudentGrades/AverageStudentGrades.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+
+int numberOFInputs = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+
+for (int i =0; i < numberOFInputs; i++)
+{
+   var input = Console.ReadLine().Split();
+
+    string name = input[0];
+    decimal grade = decimal.Parse(input[1]);
+
+    if (!students.ContainsKey(name))
+    {
+        students.Add(name, new List<decimal>());
+    }
+
+    students[name].Add(grade);
+}
+
+
+foreach(var student in students)
+{
+    Console.Write($"{student.Key} -> ");
+    
+    foreach(var grade in student.Value)
+    {
+        Console.Write($"{grade:f2} ");
+    }
+    Console.WriteLine($"(avg: {student.Value.Average():f2})");
+}
