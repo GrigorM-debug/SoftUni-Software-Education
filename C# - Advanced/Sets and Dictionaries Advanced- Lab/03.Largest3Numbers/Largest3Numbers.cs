b3cba2ba912965d54a5218e46338b670760fb1cb
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-23 12:06:19 +0300

    very interesting tasks

---
+++ C# - Advanced/Sets and Dictionaries Advanced- Lab/03.Largest3Numbers/Largest3Numbers.cs
@@ -0,0 +1,12 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+List<int> sortedNumbers = numbers.OrderByDescending(numbers=> numbers).ToList();
+
+if (sortedNumbers.Count > 3)
+{
+    Console.WriteLine(string.Join(" ", sortedNumbers.Take(3)));
+}
+else
+{
+    Console.WriteLine(string.Join(" ", sortedNumbers));
+}
---
+++ C# - Advanced/Sets and Dictionaries Advanced- Lab/06.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,13 @@
+﻿int numberOfInputs = int.Parse(Console.ReadLine());
+
+HashSet<string> names = new HashSet<string>(); 
+
+for (int i =0; i < numberOfInputs; i++)
+{
+    names.Add(Console.ReadLine());
+}
+
+foreach(var name in names)
+{
+    Console.WriteLine(name);
+}
