5fef2205081bfadee88e11d932c7fc73658b3deb
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-29 16:05:29 +0300

    i hate this because it has 1000  methods for this files directories and other bullshits

---
+++ C# - Advanced/Skeleton-Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,47 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using StreamReader readeer = new StreamReader(inputFilePath);
+
+            int row = 0;
+            string line = string.Empty;
+            char[] separator = { '-', ',', '.', '!', '?' };
+
+            StringBuilder sb = new();
+
+            while (!readeer.EndOfStream) 
+            { 
+                line = readeer.ReadLine();  
+                if (row++ % 2 == 0)
+                {
+                    foreach (char c in separator)
+                    {
+                        line = line.Replace(c, '@');
+                    }
+
+                    string[] reversed = line.Split(" ",  StringSplitOptions.RemoveEmptyEntries)
+                    .Reverse()
+                    .ToArray();
+                    
+                    sb = sb.AppendLine(string.Join(" ", reversed));
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# - Advanced/Skeleton-Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] line = File.ReadAllLines(inputFilePath);
+
+            for (int i =0; i < line.Length; i++)
+            {
+                int lettersCnt = line[i].Count(char.IsLetter);
+                int symbolsCnt = line[i].Count(char.IsPunctuation);
+
+                sb.AppendLine($"Line {i+1}: {line[i]} ({lettersCnt})({symbolsCnt})");
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+        }
+    }
+}
