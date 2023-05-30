05d693166b4ae2684900582e8dd860b942c1d4b5
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-05-30 18:41:29 +0300

    i thought that this wouldbe harder

---
+++ C# - Advanced/Functional Programming - Lab/04.AddVAT/AddVAT.cs
@@ -0,0 +1,13 @@
+﻿Func<double, double> AddVAT = n => n * 1.2;
+
+double[] numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    //.Select(n => n * 1.2)
+    .Select(n => AddVAT(n))
+    .ToArray();
+
+foreach(double number in numbers)
+{
+    Console.WriteLine($"{number:f2}");
+}
