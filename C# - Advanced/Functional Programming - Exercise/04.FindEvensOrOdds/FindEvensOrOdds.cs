702c091dc6c34fed08fd8671239b97fe5df9ac3d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-02 17:20:47 +0300

    testing and learning

--- C# - Advanced/Functional Programming - Exercise/04.FindEvensOrOdds/FindEvensOrOdds.cs
+++ C# - Advanced/Functional Programming - Exercise/04.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -1,26 +1,42 @@
-﻿using System.Text.Unicode;
+﻿using System.Data;
+using System.Text.Unicode;
 
-Predicate<int[], int, string, int> EvenOrOdd = (start, end, command)=>
+Func<int, int, List<int>> generateRange = (start, end) =>
 {
-    List<int> odd = new();
-    List<int> even = new();
+    List<int> range = new();
+
     for (int i = start; i <= end; i++)
     {
-        if (i % 2 == 0)
-        {
-            result.Add(i);
-        }
-        else if (command= "odd" && i % 2 != 0)
-        {
-            
-        }
+        range.Add(i);
     }
+
+    return range;
 };
 
+//The default value is false. IF the number is even it returs true. Else it returns false
+Predicate<int> evenOrOdd = number =>
+{
+    return number % 2 == 0;
+};
 
 
 int[] ranges = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
 
 int start = ranges[0];
 int end = ranges[1];
+
 string command = Console.ReadLine();
+
+List<int> numbers = generateRange(start, end);
+
+foreach(var number in numbers)
+{
+    if (command == "even" && evenOrOdd(number))
+    {
+        Console.Write($"{number} ");
+    }
+    else if (command == "odd" && !evenOrOdd(number))
+    {
+        Console.Write($"{number} ");
+    }
+}
---
+++ C# - Advanced/Functional Programming - Exercise/06.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+//First Solution
+//Func<List<int>, Predicate<int>, List<int>> validNumbers = (numbers, isDivisible) =>
+//{
+//    List<int> result = new List<int>();
+
+//    foreach(var number in numbers)
+//    {
+//        if (!isDivisible(number))
+//        {
+//            result.Add(number);
+//        }
+//    }
+
+//    return result;
+//};
+
+//Func<List<int>, List<int>> reverseNumbers = numbers =>
+//{
+//    List<int> result = new List<int>();
+
+//    for (int i = numbers.Count - 1; i >= 0; i--)
+//    {
+//        result.Add(numbers[i]);
+//    }
+
+//    return result;
+//};
+
+//Second solution
+Func<List<int>, Predicate<int>, List<int>> reverseNumbers = (numbers, isDivisible) =>
+{
+    List<int> result = new List<int>();
+
+    for (int i = numbers.Count - 1; i >= 0; i--)
+    {
+        if (!isDivisible(numbers[i]))
+        {
+            result.Add(numbers[i]);
+        }
+    }
+
+    return result;
+};
+
+List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+Predicate<int> isDivisible = number =>
+{
+    return number % divider == 0;
+};
+
+//numbers = validNumbers(numbers, isDivisible);
+numbers = reverseNumbers(numbers, isDivisible);
+
+Console.WriteLine($"{string.Join(" ", numbers)}");
