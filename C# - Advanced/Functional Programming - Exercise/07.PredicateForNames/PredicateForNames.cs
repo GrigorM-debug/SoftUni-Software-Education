011c49d57ccef2c5b1264b13c88a45435fd877c2
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-02 18:33:13 +0300

    i dont know why they want to solve with Func in Func instead of Predicate in Fuck

---
+++ C# - Advanced/Functional Programming - Exercise/07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,47 @@
+﻿
+//First solution
+//Func<List<string>, Predicate<string>, List<string>> validNames = (names, isValid) =>
+//{
+//    List<string> validNames = new List<string>();
+
+//    foreach(var name in names)
+//    {
+//        if (isValid(name))
+//        {
+//            validNames.Add(name);
+//        }
+//    }
+
+//    return validNames;
+//};
+
+//Second solution
+Action<List<string>, Predicate<string>> print = (names, isValid) =>
+{
+    foreach(var name in names)
+    {
+        if (isValid(name))
+        {
+            Console.WriteLine(name);
+        }
+    }
+};
+
+int length = int.Parse(Console.ReadLine());
+
+List<string> names = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+Predicate<string> isValid = name =>
+{
+    return name.Length <= length;
+};
+
+//names = validNames(names, isValid);
+
+print(names, isValid);
+
+//foreach(var name in names)
+//{
+//    Console.WriteLine(name);
+//}
---
+++ C# - Advanced/Functional Programming - Exercise/11.TriFunction/TriFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.NetworkInformation;
+
+// Logically it can be solve by prdicate but SoftUni wants Func in Func
+Func<string, int, bool> validName = (name, validSum) =>
+{
+    int nameCharSum = name.Sum(ch => ch);
+
+    return nameCharSum >= validSum;
+};
+
+Func<string[], Func<string, int, bool>, int, string> getFirstName = (names, validName, validSum) =>
+{
+    foreach(var name in names)
+    {
+        if(validName(name, validSum))
+        {
+            return name;
+        }
+    }
+
+    return null;
+};
+
+int validSum = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+string foundName = getFirstName(names, validName, validSum);
+
+Console.WriteLine(foundName);
