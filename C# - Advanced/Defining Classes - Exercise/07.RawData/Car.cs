6aafe08ed6cb4d83d91d696f7d456c590236b588
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-07 16:18:27 +0300

    I starting to understand the basics of OOP

---
+++ C# - Advanced/Defining Classes - Exercise/07.RawData/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.RawData
+{
+    public class Car
+    {
+        public Car(
+            string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, double tyre1Pressure, int tyre1Age, double tyre2Pressure, int tyre2Age, double tyre3Pressure, int tyre3Age, double tyre4Pressure, int tyre4Age
+            ) 
+        {
+            Model = model;
+            Engine = new(engineSpeed, enginePower);
+            Cargo = new(cargoWeight, cargoType);
+            Tyres = new Tyres[4];
+            Tyres[0] = new (tyre1Pressure, tyre1Age);
+            Tyres[1] = new Tyres(tyre2Pressure, tyre2Age);
+            Tyres[2] = new Tyres(tyre3Pressure, tyre3Age);
+            Tyres[3] = new Tyres(tyre4Pressure, tyre4Age);
+        }    
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tyres[] Tyres { get; set; }
+    }
+}
---
+++ C# - Advanced/Defining Classes - Exercise/07.RawData/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using _07.RawData;
+using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i< numberOfInputs; i++)
+            {
+                string[] carArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string model = carArgs[0];
+                int engineSpeed = int.Parse(carArgs[1]);
+                int enginePower = int.Parse(carArgs[2]);
+                int cargoWeight = int.Parse(carArgs[3]);
+                string cargoType = carArgs[4];
+                double tyre1Pressure = double.Parse(carArgs[5]);
+                int tyre1Age = int.Parse(carArgs[6]);
+                double tyre2Pressure = double.Parse(carArgs[7]);
+                int tyre2Age = int.Parse(carArgs[8]);
+                double tyre3Pressure = double.Parse(carArgs[9]);
+                int tyre3Age = int.Parse(carArgs[10]);
+                double tyre4Pressure = double.Parse(carArgs[11]);
+                int tyre4Age = int.Parse(carArgs[12]);
+
+                Car car = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType, tyre1Pressure, tyre1Age, tyre2Pressure, tyre2Age, tyre3Pressure, tyre3Age, tyre4Pressure, tyre4Age);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            string[] carsToPrint = null;
+
+            if(command == "fragile")
+            {
+                carsToPrint = cars.Where(c => c.Cargo.Type == "fragile" && c.Tyres.Any(t => t.Pressure < 1)).Select(c => c.Model).ToArray();
+            }
+            else if(command== "flammable")
+            {
+                carsToPrint = cars.Where(c => c.Cargo.Type == "flammable" && c.Engine.Power > 250).Select(c => c.Model).ToArray();
+            }
+
+
+            Console.WriteLine(string.Join(Environment.NewLine, carsToPrint));
+        }
+    }
+}
