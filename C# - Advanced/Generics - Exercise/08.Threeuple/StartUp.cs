e8b6360b129b1ce475da7198b27243fa9632f612
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-13 02:55:23 +0300

    I need to fix the final task tomorrow

---
+++ C# - Advanced/Generics - Exercise/08.Threeuple/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using _08.Threeuple;
+
+string[] peopleTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] beerTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] bamkTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Theeuple<string, string, string> nameAddressTown = new($"{peopleTokens[0]} {peopleTokens[1]}", peopleTokens[2], string.Join(" ", peopleTokens[3..]));
+
+Theeuple<string, int, bool> beer = new(beerTokens[0], int.Parse(beerTokens[1]), beerTokens[2] == "drunk");
+
+Theeuple<string, double, string> bank = new(bamkTokens[0], double.Parse(bamkTokens[1]), bamkTokens[2]);
+
+Console.WriteLine(nameAddressTown);
+Console.WriteLine(beer);
+Console.WriteLine(bank);
---
+++ C# - Advanced/Generics - Exercise/09.CustomLinkedList/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using _09.CustomLinkedList;
+
+DoublyLinkedList<int> list = new();
+
+list.AddFirst(3);
+list.AddFirst(2);
+list.AddFirst(1);
+list.AddLast(4);
+
+Console.WriteLine(list.RemoveFirst());
+Console.WriteLine(list.RemoveLast());
+
+int[] arr = list.ToArray();
+
+list.ForEach(i => Console.Write(i + " "));
+Console.WriteLine();
+Console.WriteLine(list.Count);
+
+DoublyLinkedList<string> stringList = new();
+
+stringList.AddFirst("some");
+stringList.AddFirst("random");
+stringList.AddFirst("strings");
+stringList.AddLast("added");
+
+stringList.ForEach(i => Console.Write(i + " "));
+Console.WriteLine();
+Console.WriteLine(stringList.Count);
+
+
+// It needs some fixes
