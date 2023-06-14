66446fc246d983afc3df0058c3565caf1118f13d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-14 19:22:03 +0300

    Practosing iterators and comparators and learning how for each is working under the hood

---
+++ C# - Advanced/Iterators and Comparators - Lab/IteratorsAndComparators/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] autors) 
+        {
+            Title = title;
+            Year = year;
+            Authors= new List<string>(autors);
+        }
+    }
+}
