4047f50396f3577cd9cfa5bb2477f50f8d12fb2d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-22 15:53:32 +0300

    Practising Inheritance

--- C# - OOP/InheritanceExercise_Skeleton_6.0/NeedForSpeed/StartUp.cs
+++ C# - OOP/InheritanceExercise_Skeleton_6.0/NeedForSpeed/StartUp.cs
@@ -1,10 +1,14 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            Car car = new Car(100, 100);
+            car.Drive(6);
+            Console.WriteLine(car.Fuel);
         }
     }
 }
---
+++ C# - OOP/InheritanceExercise_Skeleton_6.0/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+
+        public Coffee(string name, decimal price, double milliliters, double caffeine) : base(name, price, milliliters)
+        {
+            Caffeine= caffeine;
+        }
+
+        public double Caffeine { get; private set; }
+    }
+}
