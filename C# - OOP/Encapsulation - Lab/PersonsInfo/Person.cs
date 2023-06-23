7edd4a1e58d9c22d852cf2149f306dc0301a56fe
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-23 18:50:53 +0300

    This is not hard as I think before learning it

--- C# - OOP/Encapsulation - Lab/PersonsInfo/Person.cs
+++ C# - OOP/Encapsulation - Lab/PersonsInfo/Person.cs
@@ -6,23 +6,88 @@ using System.Threading.Tasks;
 
 namespace PersonsInfo
 {
+
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
         public Person(string firstName, string lastName, int age, decimal salary)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            Age = age;
-            Salary = salary;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
         }
 
-        public string FirstName { get; private set; }
+        public string FirstName 
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if(value != null && value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
 
-        public string LastName { get; private set;}
+                this.firstName = value;
+            }
+        }
 
-        public int Age { get; private set; }
+        public string LastName 
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value != null && value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
 
-        public decimal Salary { get; set; }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if(value != null && value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary 
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if(value != null && value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
 
         public void IncreaseSalary(decimal percentage)
         {
--- C# - OOP/Encapsulation - Lab/PersonsInfo/StartUp.cs
+++ C# - OOP/Encapsulation - Lab/PersonsInfo/StartUp.cs
@@ -14,14 +14,22 @@
 
                 int age = int.Parse(personArgs[2]);
                 decimal salary = decimal.Parse(personArgs[3]);
-                Person person = new Person(personArgs[0], personArgs[1], age, salary);
 
-                people.Add(person);
+                try
+                {
+                    Person person = new Person(personArgs[0],       personArgs[1], age, salary);
+
+                    people.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
 
             decimal percentage = decimal.Parse(Console.ReadLine());
 
-            foreach (var person in people)
+            foreach (Person person in people)
             {
                 person.IncreaseSalary(percentage);
                 Console.WriteLine(person.ToString());
