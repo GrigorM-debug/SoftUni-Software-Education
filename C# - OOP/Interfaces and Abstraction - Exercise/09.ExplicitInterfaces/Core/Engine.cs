cc50b80583b0ddad7399a5c9738f16987995f5a3
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-06-30 01:22:53 +0300

    The number 9 task was very very interesting. I learnt how to do Explicit interfaces Implementation

---
+++ C# - OOP/Interfaces and Abstraction - Exercise/09.ExplicitInterfaces/Core/Engine.cs
@@ -0,0 +1,36 @@
+﻿using ExplicitInterfaces.Core.Interfaces;
+using ExplicitInterfaces.Models;
+using ExplicitInterfaces.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaces.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            string input;
+            while((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                string country = tokens[1];
+                int age = int.Parse(tokens[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                person.GetName();
+                resident.GetName();
+            }
+        }
+    }
+}
