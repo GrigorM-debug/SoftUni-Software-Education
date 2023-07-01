5a31336532a5d36d369e9a39f1d9f9f56ad1048c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-07-01 18:32:07 +0300

    Task from Lection about Plymorpshism  are finished

---
+++ C# - OOP/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (this.height * 2) + (this.width * 2);
+        }
+    }
+}
