ce6e7fce9bdf2b07126c6cb4397d9a262807cf10
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-07-06 13:51:18 +0300

    practising

---
+++ C# - OOP/Polymorphism - Exercises/03.Raiding/Core/Engine.cs
@@ -0,0 +1,62 @@
+﻿using Raiding.Core.Interfaces;
+using Raiding.Factories.Interfaces;
+using Raiding.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly IHeroFactory heroFactory;
+
+       private readonly ICollection<IHero> heroes;
+
+        public Engine(IHeroFactory heroFactory, ICollection<IHero> heroes)
+        {
+            this.heroFactory = heroFactory;
+            this.heroes = heroes;
+        }
+
+        public void Run()
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            while(numberOfInputs > 0)
+            {
+                string heroName = Console.ReadLine();
+                string heroType = Console.ReadLine();
+
+                try
+                {
+                    heroes.Add(heroFactory.Create(heroType, heroName));
+                    numberOfInputs--;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach(var hero in heroes)
+            {
+                Console.WriteLine(hero.CastAbility());
+            }
+
+            int bossPower = int.Parse(Console.ReadLine());
+
+            if(heroes.Sum(x=> x.Power) >= bossPower)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+        }
+    }
+}
---
+++ C# - OOP/Polymorphism - Exercises/03.Raiding/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using Raiding.Core;
+using Raiding.Core.Interfaces;
+using Raiding.Factories.Interfaces;
+using Raiding.Factories;
+using Raiding.Models.Interfaces;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IHeroFactory heroFactory = new HeroFactory();
+            ICollection<IHero> heroes = new List<IHero>();
+
+            IEngine engine = new Engine(heroFactory, heroes);
+            engine.Run();
+        }
+    }
+}
