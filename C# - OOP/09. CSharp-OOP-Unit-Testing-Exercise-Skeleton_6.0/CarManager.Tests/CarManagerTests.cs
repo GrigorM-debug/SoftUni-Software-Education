e645daecfba708ac5e4c7aafeb5e5f217d42df68
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-07-19 16:04:12 +0300

    Unit Testing somtimes is very boring

--- C# - OOP/09. CSharp-OOP-Unit-Testing-Exercise-Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
+++ C# - OOP/09. CSharp-OOP-Unit-Testing-Exercise-Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
@@ -1,10 +1,142 @@
 namespace CarManager.Tests
 {
     using NUnit.Framework;
+    using System;
+    using System.Net.Http.Headers;
 
     [TestFixture]
     public class CarManagerTests
     {
+        [Test]
+        public void CarContructorShouldInitializeCorrectly()
+        {
+            Car car = new Car("Ford", "Mustang", 5, 40);
 
+            string expectedMake = "Ford";
+            string expectedModel = "Mustang";
+            double expectedFuelConsumption = 5;
+            double expectedFuelCapacity = 40;
+
+            Assert.AreEqual(expectedMake, car.Make);
+            Assert.AreEqual(expectedModel, car.Model); 
+            Assert.AreEqual(expectedFuelConsumption, car.FuelConsumption);
+            Assert.AreEqual(expectedFuelCapacity, car.FuelCapacity);
+        }
+
+        [Test]  
+        public void CarShouldBeCreatedWithZeroAmountOfFuel()
+        {
+            Car car = new Car("Ford", "Mustang", 5, 40);
+
+            Assert.AreEqual(0, car.FuelAmount);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void CarConsructorShouldThrowExceptionIfCarMakeIsNullOrEmpty(string make)
+        {
+            string expectedMessage = "Make cannot be null or empty!";
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => new Car(make, "Mustang", 5, 40));
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void CarConsructorShouldThrowExceptionIfCarModelIsNullOrEmpty(string model)
+        {
+            string expectedMessage = "Model cannot be null or empty!";
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => new Car("Ford", model, 5, 40));
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-29)]
+        public void CarConsructorShouldThrowExceptionIfCarFuelConsumptionIsZeroOrNegative(double fuelConsumption)
+        {
+            string expectedMessage = "Fuel consumption cannot be zero or negative!";
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => new Car("Ford", "Ford", fuelConsumption, 40));
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
+
+        [Test]
+        public void CarConsructorShouldThrowExceptionIfCarFuelAmountISNegaive()
+        {
+            Car car = new Car("Ford", "Ford", 7.5, 50.0);
+
+                                Assert.Throws<InvalidOperationException>(()
+            => car.Drive(12), "Fuel amount cannot be negative!");
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-29)]
+        public void CarConstructorShouldThrowExceptionIfFuelCapacityIsZeroOrNegative(double fuelCapacity)
+        {
+            string expectedMessage = "Fuel capacity cannot be zero or negative!";
+
+            ArgumentException x = Assert.Throws<ArgumentException>(() => new Car("Ford", "Mustang", 7.5, fuelCapacity));
+
+            Assert.AreEqual(expectedMessage, x.Message);
+        }
+
+        [Test]
+        public void RefuelMethodShouldIncreaseCarFuelAmount()
+        {
+            Car car = new Car("Ford", "Ford", 7.5, 50.0);
+
+            double expctedResult = 40;
+
+            car.Refuel(40);
+
+            Assert.AreEqual(expctedResult, car.FuelAmount);
+        }
+
+        [Test]
+        public void FuelAmountShouldBeLessThanFuelConsumption()
+        {
+            Car car = new Car("Ford", "Ford", 7.5, 50.0);
+
+            int expectedResult = 50;
+
+            car.Refuel(65);
+            double actualResult = car.FuelAmount;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void DriveMethodShouldDecreaseFuelAmount()
+        {
+            Car car = new Car("Ford", "Ford", 7.5, 50.0);
+
+            double expctedResult = 9.25;
+
+            car.Refuel(10);
+            car.Drive(10);
+
+            double actualResult = car.FuelAmount;
+
+            Assert.AreEqual(expctedResult, actualResult);
+        }
+
+        [Test]
+        public void DriveMethodShouldThrowExceptionIfYouDontHaveEnoughFuel() {
+            string expedtedMessage = "You don't have enough fuel to drive!";
+
+            Car car = new Car("Ford", "Ford", 7.5, 50.0);
+
+            car.Refuel(10);
+
+            InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => car.Drive(300));
+
+            Assert.AreEqual(expedtedMessage, ex.Message);
+        }
     }
 }
--- C# - OOP/09. CSharp-OOP-Unit-Testing-Exercise-Skeleton_6.0/FightingArena.Tests/ArenaTests.cs
+++ C# - OOP/09. CSharp-OOP-Unit-Testing-Exercise-Skeleton_6.0/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,115 @@
 ﻿namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http.Headers;
 
     [TestFixture]
     public class ArenaTests
     {
+        private Arena arena;
 
+        [SetUp]
+        public void SetUp()
+        {
+            arena = new Arena();
+        }
+
+        [Test]
+        public void ArenaConstructorShouldWorkCorrectly()
+        {
+            Assert.IsNotNull(arena);
+            Assert.IsNotNull(arena.Warriors);
+        }
+
+        [Test]
+        public void ArenaCountShouldWorkCorrectly()
+        {
+            int expectedResult = 2;
+
+            Warrior warrior = new Warrior("Ivan", 50, 100);
+            Warrior warrior1 = new Warrior("Gosho", 40, 100);
+
+            arena.Enroll(warrior);
+            arena.Enroll(warrior1);
+
+            Assert.AreEqual(expectedResult, arena.Count);   
+        }
+
+        [Test]
+        public void ArenaEnrollShouldWorkCorrectly()
+        {
+            Warrior warrior = new Warrior("Pesho", 40, 100);
+
+            arena.Enroll(warrior);
+
+            Assert.IsNotEmpty(arena.Warriors);
+            Assert.AreEqual(warrior, arena.Warriors.Single());
+        }
+
+        [Test]  
+        public void ArenaEnrollShouldThrowExceptionIfWarriorIsAlreadyEnrolled()
+        {
+            Warrior warrior = new Warrior("Ivan", 50, 100);
+
+            arena.Enroll(warrior);
+            
+            string expectedMessage = "Warrior is already enrolled for the fights!";
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException> (() => arena.Enroll(warrior));
+
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void FightMethodShouldWorkCorrectly()
+        {
+            Warrior attacker = new Warrior("Ivan", 10, 100);
+            Warrior defender = new Warrior("Pesho", 5, 100);
+
+            arena.Enroll(attacker);
+            arena.Enroll(defender);
+
+            arena.Fight(attacker.Name, defender.Name);
+
+            int expectedAttackerHp = 95;
+            int expectedDefenderHp = 90;
+
+            Assert.AreEqual(expectedAttackerHp, attacker.HP);
+
+            Assert.AreEqual(expectedDefenderHp, defender.HP);
+        }
+
+        [Test]
+        public void FightMethodShouldThrowExceptionIdAttackerNameIsNotFound()
+        {
+            Warrior attacker = new Warrior("Gosho", 10, 100);
+            Warrior defender = new Warrior("Pesho", 10, 100);
+
+            arena.Enroll(defender);
+
+            string expectedMessage = $"There is no fighter with name {attacker.Name} enrolled for the fights!";
+
+            InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => arena.Fight(attacker.Name, defender.Name));
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
+
+        [Test]
+        public void FightMethodShouldThrowExceptionIfDefenderNameIsNotFound()
+        {
+            Warrior attacker = new Warrior("Gosho", 10, 100);
+            Warrior defender = new Warrior("Pesho", 10, 100);
+
+            arena.Enroll(attacker);
+
+            string expectedMessage = $"There is no fighter with name {defender.Name} enrolled for the fights!";
+
+            InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => arena.Fight(attacker.Name, defender.Name));
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
     }
 }
