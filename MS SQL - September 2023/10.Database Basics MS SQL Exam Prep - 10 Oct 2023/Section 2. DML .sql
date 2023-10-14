8ff755f9aa5803b339d02de9d74aa32877fe5a0b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-10-14 20:31:11 +0300

    Update Section 2. DML .sql

--- MS SQL - September 2023/10.Database Basics MS SQL Exam Prep - 10 Oct 2023/Section 2. DML .sql
+++ MS SQL - September 2023/10.Database Basics MS SQL Exam Prep - 10 Oct 2023/Section 2. DML .sql
@@ -16,7 +16,7 @@ VALUES
 
 UPDATE Invoices
 SET DueDate = '2023-04-01'
-WHERE DATEPART(MONTH, DueDate) = 11 AND DATEPART(YEAR, DueDate) = 2022
+WHERE YEAR(IssueDate) = 2022 AND MONTH(IssueDate) = 11
     
 UPDATE Clients
 SET AddressId = 3
