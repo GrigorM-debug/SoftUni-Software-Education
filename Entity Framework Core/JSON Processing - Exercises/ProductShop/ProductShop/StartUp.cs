1b1636f3f1f55b98c16dfa4c72ce4f74a25040f1
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-11-15 13:29:43 +0200

    Json is fun

--- Entity Framework Core/JSON Processing - Exercises/ProductShop/ProductShop/StartUp.cs
+++ Entity Framework Core/JSON Processing - Exercises/ProductShop/ProductShop/StartUp.cs
@@ -13,9 +13,9 @@ namespace ProductShop
 
             //string inputJson = File.ReadAllText("../../../Datasets/categories-products.json");
             //Console.WriteLine(ImportCategoryProducts(context, inputJson));
-            File.WriteAllText(@"users-sold-products.json", GetSoldProducts(context));
+            File.WriteAllText(@"users-and-products.json", GetUsersWithProducts(context));
 
-            Console.WriteLine(GetSoldProducts(context));
+            Console.WriteLine(GetUsersWithProducts(context));
         }
 
         //01 - Import Users
@@ -124,6 +124,66 @@ namespace ProductShop
             };
 
             string json = JsonConvert.SerializeObject(usersWitSoldProducts, options);
+
+            return json;
+        }
+
+        //07 - Export Categories By Products Count
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories = context.Categories
+                .OrderByDescending(x => x.CategoriesProducts.Count)
+                .Select(x => new
+                {
+                    Category = x.Name,
+                    ProductsCount = x.CategoriesProducts.Count,
+                    AveragePrice = x.CategoriesProducts
+                        .Average(x => x.Product.Price).ToString("f2"),
+                    TotalRevenue = x.CategoriesProducts.Sum(x=> x.Product.Price).ToString("f2")
+                })
+                .ToArray();
+
+            var options = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+            };
+
+            var json = JsonConvert.SerializeObject(categories, options);
+
+            return json;
+        }
+
+        //08 - Export Users and Products
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(x => x.ProductsSold.Any(x => x.BuyerId != null))
+                .OrderByDescending(x => x.ProductsSold.Count)
+                .Select(x => new
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Age = x.Age,
+                    SoldProducts = x.ProductsSold.ToString(),
+                    Count = x.ProductsSold.Count,
+                        Products = x.ProductsSold
+                            .Select(p=> new
+                        {
+                            Name = p.Name,
+                            Price = p.Price
+                        }).ToArray()
+                })
+                .ToArray();
+
+            var options = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var json = JsonConvert.SerializeObject(users, options);
 
             return json;
         }
