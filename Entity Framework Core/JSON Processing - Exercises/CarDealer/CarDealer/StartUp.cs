1cd68bb7a2bf2d00999e5b1ea06b1a889fedb961
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-11-17 19:17:45 +0200

    Sometimes I hate SoftUni Judge System

--- Entity Framework Core/JSON Processing - Exercises/CarDealer/CarDealer/StartUp.cs
+++ Entity Framework Core/JSON Processing - Exercises/CarDealer/CarDealer/StartUp.cs
@@ -1,9 +1,12 @@
-﻿using System.Xml.Serialization;
+﻿using System.Globalization;
+using System.Xml.Serialization;
 using AutoMapper;
 using CarDealer.Data;
 using CarDealer.DTOs.Import;
 using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace CarDealer
 {
@@ -13,8 +16,12 @@ namespace CarDealer
         {
             var context = new CarDealerContext();
 
-            string inputJson = File.ReadAllText("../../../Datasets/sales.json");
-            Console.WriteLine(ImportSales(context, inputJson));
+            //string inputJson = File.ReadAllText("../../../Datasets/sales.json");
+            //Console.WriteLine(ImportSales(context, inputJson));
+
+            string output = GetTotalSalesByCustomer(context);
+
+            Console.WriteLine(output);
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
@@ -103,6 +110,117 @@ namespace CarDealer
             context.SaveChanges();
 
             return $"Successfully imported {sales.Length}.";
+        }
+
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .OrderBy(x => x.BirthDate)
+                .ThenBy(x => x.IsYoungDriver)
+                .Select(u => new
+                {
+                    Name = u.Name,
+                    BirthDate = u.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    IsYoungDriver = u.IsYoungDriver
+                }).ToArray();
+
+            var options = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+            };
+            string json = JsonConvert.SerializeObject(customers, options);
+
+            return json;
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Where(c=> c.Make.ToLower() == "toyota")
+                .Select(c=> new
+                {
+                    Id = c.Id,
+                    Make = c.Make,
+                    Model = c.Model,
+                    TraveledDistance = c.TraveledDistance,
+                })
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ToArray();
+
+            var options = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+            };
+            string json = JsonConvert.SerializeObject(cars, options);
+
+            return json;
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var suppliers = context.Suppliers
+                .Where(s => s.IsImporter == false)
+                .Select(s => new
+                {
+                    s.Id,
+                    s.Name,
+                    PartsCount = s.Parts.Count
+                }).ToArray();
+
+            string json = JsonConvert.SerializeObject(suppliers, Formatting.Indented);
+
+            return json;
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Select(c => new
+                {
+                    car = new
+                    {
+                        c.Make,
+                        c.Model,
+                        c.TraveledDistance
+                    },
+                    parts = c.PartsCars
+                    .Select(p=> new
+                    {
+                       Name =  p.Part.Name,
+                       Price = p.Part.Price.ToString("f2"),
+                    }).ToArray()
+                }).ToArray();
+
+            string json = JsonConvert.SerializeObject(cars, Formatting.Indented);
+
+            return json;
+        }
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .Where(c=> c.Sales.Any())
+                .Select(c=> new
+                {
+                    FullName = c.Name,
+                    BoughtCars = c.Sales.Count(),
+                    //SpendMoney = c.Sales.Sum(s => s.Car.PartsCars.Sum(pc => pc.Part.Price))
+                    SpendMoney = c.Sales.SelectMany(s => s.Car.PartsCars.Select(p => p.Part.Price)).Sum()
+                })
+                .OrderByDescending(c=> c.SpendMoney)
+                .ThenByDescending(c=> c.BoughtCars)
+                .ToList();
+
+            var option = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            string json = JsonConvert.SerializeObject(customers, option);
+
+            return json;
         }
     }
 }
