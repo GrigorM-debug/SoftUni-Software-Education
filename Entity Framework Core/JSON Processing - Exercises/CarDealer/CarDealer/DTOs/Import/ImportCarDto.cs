30c278558dac817ff1c7e115d0fdf4d184e24c04
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-11-17 16:55:37 +0200

    dd

---
+++ Entity Framework Core/JSON Processing - Exercises/CarDealer/CarDealer/DTOs/Import/ImportCarDto.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs.Import
+{
+    public class ImportCarDto
+    {
+        public ImportCarDto()
+        {
+            this.PartsIds = new HashSet<int>();
+        }
+
+        public string Make { get; set; } = null!;
+
+        public string Model { get; set; } = null!;
+
+        public int TraveledDistance { get; set; }
+        public virtual ICollection<int> PartsIds { get; set; }
+    }
+}
--- Entity Framework Core/JSON Processing - Exercises/CarDealer/CarDealer/StartUp.cs
+++ Entity Framework Core/JSON Processing - Exercises/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,86 @@
-﻿namespace CarDealer
+﻿using System.Xml.Serialization;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            var context = new CarDealerContext();
 
+            string inputJson = File.ReadAllText("../../../Datasets/cars.json");
+            Console.WriteLine(ImportCars(context, inputJson));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+
+            context.Suppliers.AddRange(suppliers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<Part[]>(inputJson);
+
+            var suppliersIds = context.Suppliers.Select(x => x.Id).ToArray();
+
+            var validParts = parts.Where(p => suppliersIds.Contains(p.SupplierId)).ToArray();
+
+            context.Parts.AddRange(validParts);
+
+           //context.SaveChanges();
+
+            return $"Successfully imported {validParts.Length}.";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            MapperConfiguration configuration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<CarDealerProfile>();
+            });
+
+            IMapper mapper = new Mapper(configuration);
+
+            ImportCarDto[] importCarDtos = JsonConvert.DeserializeObject<ImportCarDto[]>(inputJson);
+
+            //Mapping the Cars from their DTOs
+            ICollection<Car> carsToAdd = new HashSet<Car>();
+
+            foreach (var carDto in importCarDtos)
+            {
+                Car currentCar = mapper.Map<Car>(carDto);
+
+                foreach (var id in carDto.PartsIds)
+                {
+                    if (context.Parts.Any(p => p.Id == id))
+                    {
+                        currentCar.PartsCars.Add(new PartCar
+                        {
+                            PartId = id,
+                        });
+                    }
+                }
+
+                carsToAdd.Add(currentCar);
+            }
+
+            //Adding the Cars
+            context.Cars.AddRange(carsToAdd);
+            context.SaveChanges();
+
+            //Output
+            return $"Successfully imported {carsToAdd.Count}.";
         }
     }
 }
