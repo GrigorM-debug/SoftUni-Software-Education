19407b6de9d0e888b241e46145344ec0e1d2ccb8
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-11-18 16:59:31 +0200

    Making XmlHealperClass for Serialize and Desirialize

---
+++ Entity Framework Core/XML- Processing-Exercises/CarDealer/CarDealer/Utilities/XmlHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.Utilities
+{
+    public class XmlHelper
+    {
+        public static T Deserialize<T>(
+            string xmlObjectsAsString,
+            string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T[]), new XmlRootAttribute(xmlRootAttributeName));
+
+            var dataTransferObject = (T)serializer.Deserialize(new StringReader(xmlObjectsAsString));
+
+            return dataTransferObject;
+        }
+
+        public static string Serialize<T>(
+            T dataTransferObjects,
+            string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            var builder = new StringBuilder();
+
+            using var write = new StringWriter(builder);
+            serializer.Serialize(write, dataTransferObjects, GetXmlNamespaces());
+
+            return builder.ToString();
+        }
+
+        private static XmlSerializerNamespaces GetXmlNamespaces()
+        {
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+            return xmlNamespaces;
+        }
+    }
+}
