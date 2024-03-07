842f8cf3c806a5f0d6d63ee0317ad368b7763f24
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-03-07 16:11:52 +0200

    js

---
+++ JS Front End/Functions and Statements - Lab/02.MathPower.js
@@ -0,0 +1,7 @@
+function mathPower(number, pow){
+    let result = Math.pow(number, pow);
+
+    console.log(result);
+}
+
+mathPower(2,8);
---
+++ JS Front End/Functions and Statements - Lab/03.RepeatString.js
@@ -0,0 +1,8 @@
+function repeatString(word, count){
+    let result = word.repeat(Number(count));
+
+    console.log(result);
+}
+
+repeatString("abc", 3);
+
