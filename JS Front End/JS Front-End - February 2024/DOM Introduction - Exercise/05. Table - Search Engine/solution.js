d71414e705723360af2489294c6347ee775d94a4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-03-30 18:37:06 +0200

    Finished with DOM Manipulations and Events Handeling Lab

--- JS Front End/JS Front-End - February 2024/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Front End/JS Front-End - February 2024/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,22 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const inputElement = document.getElementById('searchField');
 
+      const trElements = document.querySelectorAll('table tbody tr');
+
+      Array.from(trElements)
+         .filter(trElement => {
+            const tdElements = Array.from(trElement.children);
+            const mathes = tdElements.some(td => td.textContent.toLowerCase().includes(inputElement.value.toLowerCase()));
+
+            if(mathes){
+               trElement.classList.add('select')
+            } else{
+               trElement.classList.remove('select')
+            }
+         });  
+
+      inputElement.value = '';
    }
 }
