6244755a1fd2cefdb5886a13c14dc06099ee4699
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-04-01 19:36:32 +0300

    Interesting task

--- JS Front End/JS Front-End - February 2024/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Front End/JS Front-End - February 2024/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,60 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const mainElement = document.getElementById('main');
+
+    const sendMessageDivElement = mainElement.children[0];
+    const receivedMessageDivElemenr = mainElement.children[1];
+
+    const sendMessageTextAreaElement = sendMessageDivElement.querySelector('textarea');
+    const receivedMessageTextAreaElement = receivedMessageDivElemenr.querySelector('textarea');
+
+    const encodeAndSendButtonElement = sendMessageDivElement.querySelector('button');
+    const decodeAndReadButtonElement = receivedMessageDivElemenr.querySelector('button');
+
+    mainElement.addEventListener('click', (event) => {
+        if(event.target.tagName === 'BUTTON'){
+            if(event.target.parentElement === sendMessageDivElement){
+                const sendedMessage = sendMessageTextAreaElement.value;
+                let encodedMessage = '';
+
+                for(let i = 0; i < sendedMessage.length; i++){
+                    const char = sendedMessage.charAt(i);
+                    const ascii = sendedMessage.charCodeAt(i);
+
+                    if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122) || ascii === 32) {
+                        const encodedAscii = ascii + 1;
+                        const encodedChar = String.fromCharCode(encodedAscii);
+                        encodedMessage += encodedChar;
+                    } else {
+                        encodedMessage += char;
+                    }
+                }
+                receivedMessageTextAreaElement.value = encodedMessage;
+                sendMessageTextAreaElement.value = '';
+            } else if (event.target.parentElement === receivedMessageDivElemenr) {
+                const receivedMessage = receivedMessageTextAreaElement.value;
+
+                let decodedMessage = '';
+
+                for(let i = 0; i < receivedMessage.length; i++){
+                    const char = receivedMessage.charAt(i);
+                    const ascii = receivedMessage.charCodeAt(i);
+
+                    if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)) {
+                        const decodedAscii = ascii - 1;
+
+                        const decodedChar = String.fromCharCode(decodedAscii);
+                        decodedMessage += decodedChar;
+                    } else {
+                        decodedMessage += char;
+                    }
+                }
+
+                // decodedMessage = decodedMessage.replaceAll('!', ' ');
+
+                //Because of the Judge System i must use this.
+                decodedMessage = decodedMessage.split('!').join(' ');
+                receivedMessageTextAreaElement.value = decodedMessage;
+            }   
+        }
+    });
 }
