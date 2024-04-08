7e928cd7e9afc3ae1232f9b8b0ef06616a724797
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-04-08 18:57:10 +0300

    03.Forecaster need some fixes

--- JS Front End/JS Front-End - February 2024/Asynchronous Programming-Lab/03.Forecaster/app.js
+++ JS Front End/JS Front-End - February 2024/Asynchronous Programming-Lab/03.Forecaster/app.js
@@ -1,72 +1,93 @@
 function attachEvents() {
-    const locationInputElement = document.getElementById('location');
+    const locationInputElement = document.querySelector('#location');
     const getWeatherButtonElement = document.getElementById('submit');
     const forecastElement = document.getElementById('forecast');
     const currentElement = document.getElementById('current');
     const upcomingElement = document.getElementById('upcoming');
-
-    const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations';
-
-    getWeatherButtonElement.addEventListener('click', ()=> {
-        fetch(`${baseURL}/${locationInputElement.value}`)
-            .then(response => response.json())
-            .then(locationData => {
-                const { code } = locationData.find(location => location.name === locationInputElement.value)
-                // const code = foundLocation.code;
-                console.log(code)
-
-                return Promise.all([
-                    fetch(`${baseURL}/today/${code}`),
-                    fetch(`${baseURL}/upcoming/${code}`)
-                ])
-            })
-            .then(responses => Promise.all(responses.map(res => res.json())))
-            .then(([today, upcoming]) => {
-                console.log(today)
-                console.log(upcoming)
-
-                forecastElement.style.display = 'block';
-                const forecastELEMENT = document.createElement('div');
-                forecastELEMENT.classList.add('forecasts');
-                
-                forecastELEMENT.innerHTML = `
-                    <span class='conditional symbol'>${weatherSymbols[today.forecast.condition]}</span>
-                    <span class='condition'>
-                        <span class='forecast-data'>${today.name}</span>
-                        <span class='forecast-data'>${today.forecast.low}/${today.forecast.high}</span>
-                        <span class='forecast-data'>${today.forecast.condition}</span>
-                    </span>
-                `;
-                currentElement.appendChild(forecastELEMENT);
-
-                const forecastInfoElement = document.createElement('div');
-                forecastInfoElement.classList.add('forecast-info');
-
-                forecastInfoElement.innerHTML = `
-                    <span class='upcoming'>
-                        <span class='symbol'>${weatherSymbols[upcoming.forecast.condition]}</span>
-                        <span class='condition'>
-                            <span class='forecast-data'>${upcoming.forecast.low}/${upcoming.forecast.high}</span>
-                            <span class='forecast-data'>${upcoming.forecast.condition}</span>
-                        </span>
-                    </span>
-                `;
-            
-                upcomingElement.appendChild(forecastInfoElement)
-            })
-            .catch(error => {
-                forecastElement.textContent = 'Error';
-            })
-    })
-
 
     const weatherSymbols = {
         'Sunny': '☀',
         'Partly Sunny': '⛅',
         'Overcast': '☁',
         'Rain': '☂',
-        'Degrees':	'°',
-    }
+        'Degrees': '°',
+    };
+
+    const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations';
+
+    getWeatherButtonElement.addEventListener('click', () => {
+        forecastElement.style.display = 'block';
+        fetch(`${baseURL}/${locationInputElement.value}`)
+            .then(response => response.json())
+            .then(locationData => {
+                const { code } = locationData.find(location => location.name === locationInputElement.value);
+                console.log(code);
+
+                return Promise.all([
+                    fetch(`${baseURL}/today/${code}`),
+                    fetch(`${baseURL}/upcoming/${code}`)
+                ]);
+            })
+            .then(responses => Promise.all(responses.map(res => res.json())))
+            .then(([today, upcoming]) => {
+                console.log(today)
+                console.log(upcoming)
+                // forecastElement.style.display = 'block';
+                //Current forecast element
+                // Creating current forecast element
+                const currentForecastElement = document.createElement('div');
+                currentForecastElement.classList.add('forecast');
+
+                const currentSymbolSpan = document.createElement('span');
+                currentSymbolSpan.classList.add('conditional', 'symbol');
+                currentSymbolSpan.textContent = weatherSymbols[today.forecast.condition];
+                currentForecastElement.appendChild(currentSymbolSpan);
+
+                const currentConditionSpan = document.createElement('span');
+                currentConditionSpan.classList.add('condition');
+
+                const currentNameSpan = document.createElement('span');
+                currentNameSpan.classList.add('forecast-data');
+                currentNameSpan.textContent = today.name;
+                currentConditionSpan.appendChild(currentNameSpan);
+
+                const currentTemperatureSpan = document.createElement('span');
+                currentTemperatureSpan.classList.add('forecast-data');
+                currentTemperatureSpan.textContent = `${today.forecast.low}/${today.forecast.high}`;
+                currentConditionSpan.appendChild(currentTemperatureSpan);
+
+                const ConditionSpan = document.createElement('span');
+                ConditionSpan.classList.add('forecast-data');
+                ConditionSpan.textContent = today.forecast.condition;
+                ConditionSpan.appendChild(ConditionSpan);
+
+                currentForecastElement.appendChild(currentConditionSpan);
+
+                currentElement.appendChild(currentForecastElement);
+
+
+                const forecastInfoElement = document.createElement('div');
+                forecastInfoElement.classList.add('forecast-info');
+
+                for (const upcomingForecastObj of upcoming.forecast) {
+                    const upcomingSpan = document.createElement('span');
+                    upcomingSpan.classList.add('upcoming');
+                    upcomingSpan.innerHTML = `
+                        <span class='symbol'>${weatherSymbols[upcomingForecastObj.condition]}</span>
+                        <span class='condition'>
+                            <span class='forecast-data'>${upcomingForecastObj.low}/${upcomingForecastObj.high}</span>
+                            <span class='forecast-data'>${upcomingForecastObj.condition}</span>
+                        </span>
+                    `;
+                    forecastInfoElement.appendChild(upcomingSpan);
+                }
+
+                upcomingElement.appendChild(forecastInfoElement);
+            })
+            .catch(error => {
+                forecastElement.textContent = 'Error';
+            });
+    });
 }
 
-attachEvents(); 
+attachEvents();
