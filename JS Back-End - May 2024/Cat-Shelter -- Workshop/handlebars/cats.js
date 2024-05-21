e3d069416e1864f1e00e521faa71e9021d3d192a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-05-21 17:31:27 +0300

    Adding Breeds almost done

--- JS Back-End - May 2024/Cat-Shelter -- Workshop/handlebars/cats.js
+++ JS Back-End - May 2024/Cat-Shelter -- Workshop/handlebars/cats.js
@@ -45,6 +45,33 @@ module.exports = (req, res) => {
             res.write(data);
             res.end();
         });
+    } else if(pathname === '/cats/add-breed' && req.method === 'POST'){
+        let formData = '';
+
+        req.on('data', (data) => {
+            formData += data;
+        });
+
+        req.on('end', () =>{
+            let body = qs.parse(formData)
+            
+            fs.readFile('../data/breeds.json', (err, data) =>{
+                if(err){
+                    throw err;
+                }
+
+                let breeds = JSON.parse(data); //Taking the breeds as array
+                breeds.push(body.breed);
+                let json = JSON.stringify(breeds);
+
+                fs.writeFile('../data/breeds.json', json, 'utf-8', () => console.log('The breed was uploaded successfully !'));
+            })
+
+            res.writeHead(302, { 'Location': '/' });
+            res.end();
+        })
+    } else if(pathname === '/cats/add-cat' && req.method === 'POST'){
+        
     } else {
         return true;
     }
