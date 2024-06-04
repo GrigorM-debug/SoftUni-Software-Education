091c47fb69686961ce611d59771c37e8adbf8765
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-04 18:33:32 +0300

    Update the Search to be partial. You can search the movie without writing the whole title, genre.

--- JS Back-End - May 2024/Movie Catalog/src/controllers/homeController.js
+++ JS Back-End - May 2024/Movie Catalog/src/controllers/homeController.js
@@ -26,25 +26,39 @@ module.exports = {
         const movieGenre = req.query.genre;
         const movieYear = Number(req.query.year);
 
-        if(movieTitle && !movieGenre && !movieYear){
-            moviesFiltered = movies.filter(m => m.title === movieTitle);
+        // if(movieTitle && !movieGenre && !movieYear){
+        //     moviesFiltered = movies.filter(m => m.title === movieTitle);
 
-            console.log(moviesFiltered)
-            // res.render('search', {movies});
-        } else if (!movieTitle && movieGenre && !movieYear){
-            moviesFiltered = movies.filter(m => m.genre === movieGenre);
+        //     console.log(moviesFiltered)
+        //     // res.render('search', {movies});
+        // } else if (!movieTitle && movieGenre && !movieYear){
+        //     moviesFiltered = movies.filter(m => m.genre === movieGenre);
 
-            console.log(moviesFiltered)
-            // res.render('search', {movies});
-        } else if (!movieTitle && !movieGenre && movieYear){
+        //     console.log(moviesFiltered)
+        //     // res.render('search', {movies});
+        // } else if (!movieTitle && !movieGenre && movieYear){
+        //     moviesFiltered = movies.filter(m => m.year === movieYear);
+
+        //     console.log(moviesFiltered)
+        //     // res.render('search', {movies});
+        // } else{
+        //     moviesFiltered = movies.filter(m => m.title === movieTitle && m.genre === movieGenre && m.year === movieYear);
+
+        //     console.log(moviesFiltered)
+        // }
+
+        if (movieTitle && !movieGenre && !movieYear) {
+            moviesFiltered = movies.filter(m => new RegExp(movieTitle, 'i').test(m.title));
+            console.log('Filtered by title:', moviesFiltered);
+        } else if (!movieTitle && movieGenre && !movieYear) {
+            moviesFiltered = movies.filter(m => new RegExp(movieGenre, 'i').test(m.genre));
+            console.log('Filtered by genre:', moviesFiltered);
+        } else if (!movieTitle && !movieGenre && movieYear) {
             moviesFiltered = movies.filter(m => m.year === movieYear);
-
-            console.log(moviesFiltered)
-            // res.render('search', {movies});
-        } else{
-            moviesFiltered = movies.filter(m => m.title === movieTitle && m.genre === movieGenre && m.year === movieYear);
-
-            console.log(moviesFiltered)
+            console.log('Filtered by year:', moviesFiltered);
+        } else {
+            moviesFiltered = movies.filter(m => new RegExp(movieTitle, 'i').test(m.title) && new RegExp(movieGenre, 'i').test(m.genre) && m.year === movieYear);
+            console.log('Filtered by title, genre, and year:', moviesFiltered);
         }
 
         res.render('search', {movies: moviesFiltered});
