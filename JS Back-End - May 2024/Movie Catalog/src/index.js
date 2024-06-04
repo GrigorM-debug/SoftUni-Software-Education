a6859fff76acd236c9ac2a1fc66588a060401070
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-04 16:09:17 +0300

    Start using mongoDb database with mongoose

--- JS Back-End - May 2024/Movie Catalog/src/index.js
+++ JS Back-End - May 2024/Movie Catalog/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { hbsConfig } = require('../config/hbsConfig');
 const {configExpress} = require('../config/expressConfig');
 const { router } = require('../config/routes');
+const { databaseConfig } = require('../config/databaseConfig');
 
 const app = express();
 const PORT = 3000;
 
 configExpress(app);
 hbsConfig(app);
+databaseConfig();
 app.use(router)
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
