c39aebc2855b166cbdf7054c5d853fb3af904e58
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-17 23:28:37 +0300

    Add mongoose validation in Cast Schema

--- JS Back-End - May 2024/Movie Catalog/src/models/Cast.js
+++ JS Back-End - May 2024/Movie Catalog/src/models/Cast.js
@@ -3,17 +3,21 @@ const {Schema, model, SchemaTypes} = require('mongoose');
 const castSchema = new Schema({
     name: {
         type: String,
-        require: true
+        require: true,
+        min: [5, 'Cast name must be at least 5 characters long.'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Please enter a valid name!']
     },
     age: {
         type: Number,
         require: true,
-        min: 14,
+        min: 1,
         max: 120
     },
     born: {
         type: String,
-        require: true
+        require: true,
+        min: 10,
+        match: [/^[a-zA-Z0-9\s]+$/, 'Please enter a valid date!']
     },
     nameInMovie: {
         type: String,
@@ -22,12 +26,7 @@ const castSchema = new Schema({
     castImage: {
         type: String,
         require: true,
-        validate: {
-            validator: function(value) {
-                return /^(http|https):\/\/[^ "]+$/.test(value);
-            },
-            message: props => `${props.value} is not a valid URL!`
-        }
+        match: [/^(http|https):\/\/[^ "]+$/, 'Please enter a valid URL!']
     },
     movies: [{
         type: SchemaTypes.ObjectId, 
