1f1a106a7963f5415c59a6786a796808ddbffd93
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-20 20:15:32 +0300

    add Mongoose Schema validation for Volcano

--- JS Back-End - May 2024/Exam Preparation/JS Back-End Retake Exam – Magma Haven/src/models/Volcano.js
+++ JS Back-End - May 2024/Exam Preparation/JS Back-End Retake Exam – Magma Haven/src/models/Volcano.js
@@ -3,32 +3,42 @@ const {Schema, model, SchemaTypes} = require('mongoose');
 const volcanoSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required'],
+        min: [2, 'Name must be at least 2 characters long !']
     },
     location: {
         type: String,
-        required: true
+        required: [true, 'Location is required !'],
+        min: [3, 'Location must be at least 3 characters long !']
     },
     elevation: {
         type: Number,
-        required: true
+        required: [true, 'Elevation is required !'],
+        min: [0, 'Elevation must be at least 0 !']
     },
     lastEruption: {
         type: Number,
-        require: true
+        require: [true, 'Year of Last Eruption is required !'],
+        min: [0, "Year of Last Eruption must at least 0 !"],
+        max: [2024, "Year of Last Eruption must at least 0 !"]
     },
     image: {
         type: String,
-        required: true
+        required: [true, 'Image is required'],
+        match: [/^(http:\/\/|https:\/\/)/, 'Invalid image URL!']
     },
     typeVolcano: {
         type: String,
-        required: true,
-        enum: ["Supervolcanoes", "Submarine", "Subglacial", "Mud", "Stratovolcanoes", "Shield"]
+        required: [true, 'Volcano type is required !'],
+        enum: {
+            values: ["Supervolcanoes", "Submarine", "Subglacial", "Mud", "Stratovolcanoes", "Shield"],
+            message: 'The value `{VALUE}` is not supported for the volcano type.'
+        }
     },
     description: {
         type: String,
-        required: true
+        required: [true, 'Description is required !'],
+        min: [10, 'Description must at least 10 characters long !']
     },
     voteList: [{
         type: SchemaTypes.ObjectId,
