4b186b117e25973179f0d497300f8fee32a30992
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-21 00:05:06 +0300

    Search is done

--- JS Back-End - May 2024/Exam Preparation/JS Back-End Retake Exam – Magma Haven/src/controllers/searchController.js
+++ JS Back-End - May 2024/Exam Preparation/JS Back-End Retake Exam – Magma Haven/src/controllers/searchController.js
@@ -2,18 +2,23 @@ const { getAllVolcanos } = require("../services/volcano");
 
 module.exports = {
     searchController: async (req, res) => {
-        const volcanos = await getAllVolcanos().lean();
+        const volcanosSearch = await getAllVolcanos().lean();
         let volcanosFiltered = [];
         const volcanoName = req.query.name;
         const volcanoType = req.query.type;
 
         //Partial search thanks to ChatGpt
         if (volcanoName && !volcanoType) {
-            volcanosFiltered = volcanos.filter(v => new RegExp(volcanoName, 'i').test(v.name));
+            volcanosFiltered = volcanosSearch.filter(v => new RegExp(volcanoName, 'i').test(v.name));
         } else if (!volcanoName && volcanoType) {
-            volcanosFiltered = volcanos.filter(v => new RegExp(volcanoType, 'i').test(v.volcanoType));
+            volcanosFiltered = volcanosSearch.filter(v => new RegExp(volcanoType, 'i').test(v.typeVolcano));
+        } else if (volcanoName && volcanoType) {
+            volcanosFiltered = volcanosSearch.filter(v => 
+                new RegExp(volcanoName, 'i').test(v.name) && 
+                new RegExp(volcanoType, 'i').test(v.typeVolcano)
+            );
         } else {
-            volcanosFiltered = volcanos.filter(v => new RegExp(volcanoName, 'i').test(v.name) && new RegExp(volcanoType, 'i').test(v.type));
+            volcanosFiltered = []; // No filters applied, return empty array
         }
 
         res.render('search', {volcanos: volcanosFiltered});
