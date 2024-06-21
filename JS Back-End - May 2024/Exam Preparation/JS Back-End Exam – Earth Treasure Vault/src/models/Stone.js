2702adb35241f4285379c8e7b4c2b2c2ab320d5b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-21 14:40:18 +0300

    Stone Schema is done

---
+++ JS Back-End - May 2024/Exam Preparation/JS Back-End Exam – Earth Treasure Vault/src/models/Stone.js
@@ -0,0 +1,45 @@
+const {Schema, SchemaTypes, model} = require('mongoose');
+
+const stoneSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required !']
+    },
+    category : {
+        type: String,
+        required: [true, 'Category  is required !']
+    },
+    color: {
+        type: String,
+        required: [true, 'Color is required !']
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required !']
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required !']
+    },
+    formula: {
+        type: String,
+        required: [true, 'Formula is required !']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required !']
+    },
+    likedList: [{
+        type: SchemaTypes.ObjectId,
+        ref: 'User',
+        default: []
+    }],
+    owner: {
+        type: SchemaTypes.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Stone = model('Stone', stoneSchema);
+
+module.exports = {Stone};
