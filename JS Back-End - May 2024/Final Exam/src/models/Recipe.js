62a7286f550b8fcf12f6c7fb8c3bca4284ddd33f
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-23 17:12:35 +0300

    Update the error messages in mongoose Schemas

--- JS Back-End - May 2024/Final Exam/src/models/Recipe.js
+++ JS Back-End - May 2024/Final Exam/src/models/Recipe.js
@@ -4,24 +4,24 @@ const recipeSchema = new Schema({
     title: {
         type: String, 
         required: [true, 'Title is required !'],
-        min: [2, 'Title must at least 2 characters long !']
+        min: [2, 'Title should at least 2 characters long !']
     },
     ingredients: {
         type: String,
         required: [true, 'Ingredients are required !'],
-        min: [10, 'Ingredients must between 10 and 200 characters long !'],
-        max: [200, 'Ingredients must between 10 and 200 characters long !']
+        min: [10, 'Ingredients should between 10 and 200 characters long !'],
+        max: [200, 'Ingredients should between 10 and 200 characters long !']
     },
     instructions: {
         type: String,
         required: [true, 'Instructions are required !'],
-        min: [10, 'Instructions must be at least 10 characters long !']
+        min: [10, 'Instructions should be at least 10 characters long !']
     },
     description: {
         type: String,
         required: [true, 'Description is required !'],
-        min: [10, 'Description must between 10 and 100 characters long !'],
-        max: [100, 'Description must between 10 and 100 characters long !']
+        min: [10, 'Description should between 10 and 100 characters long !'],
+        max: [100, 'Description should between 10 and 100 characters long !']
     },
     image: {
         type: String,
--- JS Back-End - May 2024/Final Exam/src/models/User.js
+++ JS Back-End - May 2024/Final Exam/src/models/User.js
@@ -1,22 +1,22 @@
 const {Schema, SchemaTypes, model} = require('mongoose');
 
 const userSchema = new Schema({
-    username: {
+    name: {
         type: String,
-        required: [true, 'Username is required !'],
-        min: [2, 'Username must be between 2 and 20 characters long !'],
-        max: [20, 'Username must be between 2 and 20 characters long !']
+        required: [true, 'Name is required !'],
+        min: [2, 'Name should be between 2 and 20 characters long !'],
+        max: [20, 'Name should be between 2 and 20 characters long !']
     },
     email: {
         type: String,
         required: [true, 'Email is required !'],
         unique: true,
-        min: [10, 'Email must be at least 10 characters long !']
+        min: [10, 'Email should be at least 10 characters long !']
     },
     password: {
         type: String,
         required: [true, 'Password is required !'],
-        min: [4, 'Password must at least 4 chracters long !']
+        min: [4, 'Password should at least 4 chracters long !']
     }
 });
 
