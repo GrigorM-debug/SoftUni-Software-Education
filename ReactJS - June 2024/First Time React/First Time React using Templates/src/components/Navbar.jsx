bdcfa742c9f09de40461caf60c37dc811e77c095
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-27 23:16:42 +0300

    Making some React project using HTML and CSS Template just to practice the installation and Components

---
+++ ReactJS - June 2024/First Time React/First Time React using Templates/src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg">
+        <div className="container">
+        <a className="navbar-brand d-flex align-items-center" href="index.html">
+            <img
+            src="/images/coffee-beans.png"
+            className="navbar-brand-image img-fluid"
+            alt="Barista Cafe Template"
+            />
+            Barista
+        </a>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-lg-auto">
+            <li className="nav-item">
+                <a className="nav-link click-scroll" href="#section_1">
+                Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link click-scroll" href="#section_2">
+                About
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link click-scroll" href="#section_3">
+                Our Menu
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link click-scroll" href="#section_4">
+                Reviews
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link click-scroll" href="#section_5">
+                Contact
+                </a>
+            </li>
+            </ul>
+            <div className="ms-lg-3">
+            <a
+                className="btn custom-btn custom-border-btn"
+                href="reservation.html"
+            >
+                Reservation
+                <i className="bi-arrow-up-right ms-2" />
+            </a>
+            </div>
+        </div>
+        </div>
+    </nav>
+    );
+}
+
+export default Navbar;
---
+++ ReactJS - June 2024/First Time React/First Time React using Templates/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import '../public/css/bootstrap-icons.css';
+import '../public/css/bootstrap.min.css';
+import '../public/css/tooplate-barista.css';
+import '../public/css/vegas.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
