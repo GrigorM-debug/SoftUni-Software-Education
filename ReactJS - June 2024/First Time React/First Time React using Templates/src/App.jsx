930b326c23e9a3004f060cec8803ab24d5f4add6
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-06-28 00:19:16 +0300

    I think is finished. I did it only to practice basic React Components

--- ReactJS - June 2024/First Time React/First Time React using Templates/src/App.jsx
+++ ReactJS - June 2024/First Time React/First Time React using Templates/src/App.jsx
@@ -1,13 +1,23 @@
+import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Menu from './components/Menu';
 import Navbar from './components/Navbar';
+import Reviews from './components/Reviews';
+import Team from './components/Team';
+import Welcome from './components/Welcome';
 
 function App() {
 
   return (
     <>
       <Navbar/>
-
-
+      <Welcome/>
+      <About/>
+      <Team/>
+      <Menu/>
+      <Reviews/>
+      <Contact/>
       <Footer/>
     </>
   )
--- ReactJS - June 2024/First Time React/First Time React using Templates/src/components/Navbar.jsx
+++ ReactJS - June 2024/First Time React/First Time React using Templates/src/components/Navbar.jsx
@@ -1,66 +1,68 @@
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg">
-        <div className="container">
-        <a className="navbar-brand d-flex align-items-center" href="index.html">
-            <img
-            src="/images/coffee-beans.png"
-            className="navbar-brand-image img-fluid"
-            alt="Barista Cafe Template"
-            />
-            Barista
-        </a>
-        <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-lg-auto">
-            <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_1">
-                Home
+        <div id="sticky-wrapper" className="sticky-wrapper" style={{height: '154px'}}>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                <a className="navbar-brand d-flex align-items-center" href="index.html">
+                    <img
+                    src="/images/coffee-beans.png"
+                    className="navbar-brand-image img-fluid"
+                    alt="Barista Cafe Template"
+                    />
+                    Barista
                 </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_2">
-                About
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_3">
-                Our Menu
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_4">
-                Reviews
-                </a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_5">
-                Contact
-                </a>
-            </li>
-            </ul>
-            <div className="ms-lg-3">
-            <a
-                className="btn custom-btn custom-border-btn"
-                href="reservation.html"
-            >
-                Reservation
-                <i className="bi-arrow-up-right ms-2" />
-            </a>
-            </div>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-lg-auto">
+                    <li className="nav-item">
+                        <a className="nav-link click-scroll" href="#section_1">
+                        Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link click-scroll" href="#section_2">
+                        About
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link click-scroll" href="#section_3">
+                        Our Menu
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link click-scroll" href="#section_4">
+                        Reviews
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link click-scroll" href="#section_5">
+                        Contact
+                        </a>
+                    </li>
+                    </ul>
+                    <div className="ms-lg-3">
+                    <a
+                        className="btn custom-btn custom-border-btn"
+                        href="reservation.html"
+                    >
+                        Reservation
+                        <i className="bi-arrow-up-right ms-2" />
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </nav>
         </div>
-        </div>
-    </nav>
     );
 }
 
